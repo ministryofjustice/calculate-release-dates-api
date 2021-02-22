c566ce7f972ce4ad44c3870c75095f867726feb9
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2021-02-22 19:23:06 +0000

    DT-1638 useLatestVersions (#19)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.1"
   kotlin("plugin.spring") version "1.4.30"
 }
 
