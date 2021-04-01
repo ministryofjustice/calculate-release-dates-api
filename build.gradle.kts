afaee1c4631028d9f8364466bc7fa562a2109851
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2021-04-01 11:49:35 +0100

    DT-1829 Upgrades (#30)
    * DT-1829 useLatestVersions
    
    * DT-1829 upgrade kotlin spring version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.1"
-  kotlin("plugin.spring") version "1.4.30"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.4"
+  kotlin("plugin.spring") version "1.4.32"
 }
 
 configurations {
