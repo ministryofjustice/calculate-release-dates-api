4c4e36ea4e8ef44694222253878e188208e09290
petergphillips <peter@greenthistle.co.uk>
2021-04-09 07:47:11 +0100

    DT-1858: 🐛 Suppress netty false positive (#31)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.6"
   kotlin("plugin.spring") version "1.4.32"
 }
 
