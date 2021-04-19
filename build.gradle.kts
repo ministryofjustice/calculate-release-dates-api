b0c1e2f88245870ace74e33780ee2580438b96e6
petergphillips <peter@greenthistle.co.uk>
2021-04-19 13:44:08 +0100

    DT-1893: ✨ Specify jvm target and circle use java 16 (#33)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.6"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.7"
   kotlin("plugin.spring") version "1.4.32"
 }
 
@@ -9,4 +9,12 @@ configurations {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
+}
+
+tasks {
+  compileKotlin {
+    kotlinOptions {
+      jvmTarget = "15"
+    }
+  }
 }
