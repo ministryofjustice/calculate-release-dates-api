eee9db3b6b615ed144ca7e5639663e0af0ed5452
petergphillips <peter@greenthistle.co.uk>
2021-05-07 08:20:20 +0100

    DT-1987: ⬆️  Upgrade to kotlin 1.5 (#36)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.7"
-  kotlin("plugin.spring") version "1.4.32"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.2.0-beta"
+  kotlin("plugin.spring") version "1.5.0"
 }
 
 configurations {
@@ -14,7 +14,7 @@ dependencies {
 tasks {
   compileKotlin {
     kotlinOptions {
-      jvmTarget = "15"
+      jvmTarget = "16"
     }
   }
 }
