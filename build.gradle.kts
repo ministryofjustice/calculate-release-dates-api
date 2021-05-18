65a4de5ea582c8342200fe5053a98619efca3acd
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2021-05-18 16:07:15 +0100

    DT-2050: ⬆️ Upgrade to app insights 3.1.0 (#40)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.2.0-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.2.1"
   kotlin("plugin.spring") version "1.5.0"
 }
 
