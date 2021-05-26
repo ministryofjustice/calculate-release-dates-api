9cdf4f1cc2036f38fa4dc97170f6f16617e5eca0
petergphillips <peter@greenthistle.co.uk>
2021-05-26 10:13:04 +0100

    pgp DT 2070 spring boot upgrade (#41)
    * DT-2070: ⬆️ Upgrade to spring boot 2.5.0 and kotlin 1.5.10
    
    * DT-2070: ⬆️ Upgrade to 3.3.0 of gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.2.1"
-  kotlin("plugin.spring") version "1.5.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.0"
+  kotlin("plugin.spring") version "1.5.10"
 }
 
 configurations {
