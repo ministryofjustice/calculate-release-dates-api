6c18242ae3e789419e2536068dff657e157e58ab
Joel Stobart (MOJ) <87521440+joelstobart-moj@users.noreply.github.com>
2021-07-21 14:08:30 +0100

    Files in values and values-dev (#5)
    * Files in values and values-dev
    
    * Files in yaml
    
    * Addition of Local Public Key to match additions!
    
    * Fixes to the application.yml
    
    * Fixes to the application.yml for logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.4"
-  kotlin("plugin.spring") version "1.5.20"
+  kotlin("plugin.spring") version "1.5.21"
+  kotlin("plugin.jpa") version "1.5.21"
 }
 
 configurations {
