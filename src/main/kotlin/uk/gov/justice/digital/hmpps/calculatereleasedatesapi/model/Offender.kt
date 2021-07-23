ce2bd1579daa272904ff50549cce8424338d8254
Joel Stobart (MOJ) <joel.stobart@digital.justice.gov.uk>
2021-07-23 11:36:58 +0100

    Amend the offender to use Val rather then var, remove the public, and the getters and setters

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/Offender.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/Offender.kt
@@ -1,9 +1,6 @@
 package uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model
 
 class Offender(
-  private var referenceField: String,
-  private var nameField: String
-) {
-  val name get() = nameField
-  val reference get() = referenceField
-}
+  val reference: String,
+  val name: String
+)
