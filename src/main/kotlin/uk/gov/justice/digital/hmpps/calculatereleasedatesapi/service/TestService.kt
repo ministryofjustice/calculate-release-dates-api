366d83cc9d0260c6a2919254a663010a5618883b
Yasin Mustafa <yasin.mustafa@gmail.com>
2021-08-09 20:05:08 +0100

    CRS-172 Integrate the prison-api with the service and implement a test call (#34)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/TestService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/TestService.kt
@@ -5,9 +5,15 @@ import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.TestData
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.repository.TestRepository
 
 @Service
-class TestService(private val testRepository: TestRepository) {
+class TestService(
+  private val testRepository: TestRepository,
+  private val prisonService: PrisonService,
+) {
 
   fun getTestData(): List<TestData> {
-    return testRepository.findAll().map { transform(it) }
+    val prisoner = prisonService.getOffenderDetail("A1234AA")
+    val testData = testRepository.findAll().map { transform(it) }.toMutableList()
+    testData.add(TestData(prisoner.offenderNo, prisoner.bookingId))
+    return testData
   }
 }
