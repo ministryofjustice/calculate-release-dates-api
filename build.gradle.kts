e4ceaf640dd95dbc85e06a972d7ab98fec1aff91
Jon Brighton <jonathan.brighton@digital.justice.gov.uk>
2021-09-28 16:21:04 +0100

    DT-2619: ⬆️ Upgrading to Spring Boot 2.5.5 to resolve CVE (#66)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.8"
-  kotlin("plugin.spring") version "1.5.30"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.9"
+  kotlin("plugin.spring") version "1.5.31"
 }
 
 configurations {
