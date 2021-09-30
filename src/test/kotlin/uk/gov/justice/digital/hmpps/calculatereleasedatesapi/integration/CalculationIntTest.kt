fdef61cfce35ee7e95983877cdc88bc5a4cbcc67
Yasin Mustafa <yasin.mustafa@gmail.com>
2021-09-30 09:25:42 +0100

    CRS-441 Create a POST endpoint to confirm a calculation of release dates and publish an event to domain-topic  (#64)
    * CRS-441 Create a POST endpoint to confirm a calculation of release dates and publish an event to domain-topic
    
    * CRS-441 Create a POST endpoint to confirm a calculation of release dates and publish an event to domain-topic

--- src/test/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/integration/CalculationIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/integration/CalculationIntTest.kt
@@ -38,4 +38,28 @@ class CalculationIntTest : IntegrationTestBase() {
     assertThat(calculationRequest.inputData["offender"]["reference"].asText()).isEqualTo("A1234AA")
     assertThat(calculationRequest.inputData["sentences"][0]["offence"]["committedAt"].asText()).isEqualTo("2015-03-17")
   }
+
+  @Test
+  fun `Confirm a calculation for a prisoner (based on example 13 from the unit tests) + test input JSON in DB`() {
+    val result = webTestClient.post()
+      .uri("/calculation/A1234AA/confirm")
+      .accept(MediaType.APPLICATION_JSON)
+      .headers(setAuthorisation(roles = listOf("ROLE_CRD_ADMIN")))
+      .exchange()
+      .expectStatus().isOk
+      .expectHeader().contentType(MediaType.APPLICATION_JSON)
+      .expectBody(BookingCalculation::class.java)
+      .returnResult().responseBody
+
+    val calculationRequest = calculationRequestRepository.findById(result.calculationRequestId)
+      .orElseThrow { EntityNotFoundException("No calculation request exists for id ${result.calculationRequestId}") }
+
+    assertThat(result).isNotNull
+    assertThat(result.dates[SLED]).isEqualTo(LocalDate.of(2016, 11, 6))
+    assertThat(result.dates[CRD]).isEqualTo(LocalDate.of(2016, 1, 6))
+    assertThat(result.dates[TUSED]).isEqualTo(LocalDate.of(2017, 1, 6))
+    assertThat(calculationRequest.calculationStatus).isEqualTo("CONFIRMED")
+    assertThat(calculationRequest.inputData["offender"]["reference"].asText()).isEqualTo("A1234AA")
+    assertThat(calculationRequest.inputData["sentences"][0]["offence"]["committedAt"].asText()).isEqualTo("2015-03-17")
+  }
 }
