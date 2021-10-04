b9d49e11771ab2cb177ac4411c5692126b0a54e5
yasin <yasin.mustafa@gmail.com>
2021-10-04 09:24:48 +0100

    CRS-274 Lint fixes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/repository/CalculationRequestRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/repository/CalculationRequestRepository.kt
@@ -3,7 +3,7 @@ package uk.gov.justice.digital.hmpps.calculatereleasedatesapi.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.entity.CalculationRequest
-import java.util.*
+import java.util.Optional
 
 @Repository
 interface CalculationRequestRepository : JpaRepository<CalculationRequest, Long> {
