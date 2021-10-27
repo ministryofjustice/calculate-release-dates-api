b8b901caa15793f0364a6a09b8d12d2cbe6b52fc
Joel Stobart (MOJ) <joel.stobart@digital.justice.gov.uk>
2021-10-27 09:01:53 +0100

    CRS-486 Small security fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.10-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.12-beta"
   kotlin("plugin.spring") version "1.5.30"
   kotlin("plugin.jpa") version "1.5.30"
   id("io.gitlab.arturbosch.detekt").version("1.18.0-RC2")
