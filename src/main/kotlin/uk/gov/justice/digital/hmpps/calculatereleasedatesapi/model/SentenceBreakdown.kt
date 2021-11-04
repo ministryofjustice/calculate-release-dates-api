1209f805996a568538feffcd2b6a3ee1f1457545
Luke Harper <luke.harper@digital.justice.gov.uk>
2021-11-04 13:57:30 +0000

    CRS-529 reformat break.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/SentenceBreakdown.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/SentenceBreakdown.kt
@@ -3,7 +3,7 @@ package uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.enumerations.SentenceType
 import java.time.LocalDate
 
-interface SentenceBreakdown: SentenceLengthBreakdown {
+interface SentenceBreakdown : SentenceLengthBreakdown {
   val sentencedAt: LocalDate
   val dates: Map<SentenceType, DateBreakdown>
 }
