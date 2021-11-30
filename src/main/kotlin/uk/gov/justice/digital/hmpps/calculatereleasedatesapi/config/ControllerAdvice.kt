f9e726fec02c2123b998c856668774a25b69041e
Luke Harper <luke.harper@digital.justice.gov.uk>
2021-11-30 11:22:24 +0000

    CRS-466 changing the return of the error message.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/config/ControllerAdvice.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/config/ControllerAdvice.kt
@@ -127,13 +127,9 @@ class ControllerAdvice {
       .body(
         ErrorResponse(
           status = HttpStatus.BAD_REQUEST,
-          userMessage = """
-            One or more of the sentence types in this calculation is not currently supported in this service:
-            
-            ${e.sentenceAndOffences.map { it.sentenceTypeDescription }.distinct().joinToString(separator = "\n")}
-            
-            If these sentences are correct, you will need to complete this calculation manually in NOMIS.
-          """.trimIndent(),
+          errorCode = 1,
+          userMessage = "One or more of the sentence types in this calculation is not currently supported in this service",
+          arguments = e.sentenceAndOffences.map { it.sentenceTypeDescription }.distinct(),
           developerMessage = e.message
         )
       )
@@ -160,13 +156,15 @@ data class ErrorResponse(
   val userMessage: String? = null,
   val developerMessage: String? = null,
   val moreInfo: String? = null,
+  val arguments: List<String> = listOf()
 ) {
   constructor(
     status: HttpStatus,
     errorCode: Int? = null,
     userMessage: String? = null,
     developerMessage: String? = null,
-    moreInfo: String? = null
+    moreInfo: String? = null,
+    arguments: List<String> = listOf()
   ) :
-    this(status.value(), errorCode, userMessage, developerMessage, moreInfo)
+    this(status.value(), errorCode, userMessage, developerMessage, moreInfo, arguments)
 }
