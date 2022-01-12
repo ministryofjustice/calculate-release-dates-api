2b1a85707f162a65ec324315aceb0bb6a93988de
Yasin Mustafa <yasin.mustafa@digital.justice.gov.uk>
2022-01-12 14:08:36 +0000

    CRS-672 Upgrade spring boot plugin to 4.0.1 (#134)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.1-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.1"
   kotlin("plugin.spring") version "1.6.10"
   kotlin("plugin.jpa") version "1.6.10"
   id("io.gitlab.arturbosch.detekt").version("1.18.0-RC2")
