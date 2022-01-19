1e0ab5158631ca220b93b6d42be7f5cc48d31446
timharrison-moj <47779388+timharrison-moj@users.noreply.github.com>
2022-01-19 07:34:09 +0000

    CVSL-337: Update spring boot plugin to remove owasp/trivy failures overnight. (#80)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.0"
-  kotlin("plugin.spring") version "1.6.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.1"
+  kotlin("plugin.spring") version "1.6.10"
 }
 
 configurations {
