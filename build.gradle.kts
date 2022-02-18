81b4a564a4565497995195dd9c610c2c0b8f83d1
Joel Stobart (MOJ) <87521440+joelstobart-moj@users.noreply.github.com>
2022-02-18 15:05:36 +0000

    CRS-750 increase the jdk version to 17 (#160)
    * Changes to make service use JDK17
    Update package versions
    Update JVM target
    Update HMPPS Orb version
    Update docker openjdk version
    
    * Small change to handle version change
    in kotlin and java versions to 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.3-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.3"
   kotlin("plugin.spring") version "1.6.10"
   kotlin("plugin.jpa") version "1.6.10"
   id("io.gitlab.arturbosch.detekt").version("1.18.0-RC2")
@@ -65,24 +65,24 @@ dependencies {
   implementation("com.vladmihalcea:hibernate-types-52:2.14.0")
 
   // OpenAPI
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.4")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.3")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.3")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.5")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.5")
 
   implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.0.8")
-  implementation("com.amazonaws:aws-java-sdk-sns:1.12.146")
+  implementation("com.amazonaws:aws-java-sdk-sns:1.12.154")
   implementation("org.springframework.cloud:spring-cloud-aws-messaging:2.2.6.RELEASE")
   implementation("org.springframework:spring-jms")
-  implementation("com.google.code.gson:gson:2.8.9")
+  implementation("com.google.code.gson:gson:2.9.0")
 
   // Test dependencies
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("org.awaitility:awaitility-kotlin:4.1.1")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.28.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.31.0")
   testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.0.29")
-  testImplementation("org.mockito:mockito-inline:4.2.0")
+  testImplementation("org.mockito:mockito-inline:4.3.1")
   testImplementation("io.projectreactor:reactor-test")
   testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
   testImplementation("com.h2database:h2")
@@ -96,14 +96,12 @@ jacoco {
   toolVersion = "0.8.7"
 }
 
-tasks {
-  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-      jvmTarget = "16"
-    }
+kotlin {
+  jvmToolchain {
+    (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of("17"))
   }
 }
 
 java {
-  toolchain.languageVersion.set(JavaLanguageVersion.of(16))
+  toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
