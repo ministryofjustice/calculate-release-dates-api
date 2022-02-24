8046e507784cd3f2e7bbccd90ceb8a1b302bdac9
petergphillips <peter@greenthistle.co.uk>
2022-02-24 08:31:49 +0000

    SDI-71: 🔒️  Bump versions for security issues (#86)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.4"
   kotlin("plugin.spring") version "1.6.10"
 }
 
