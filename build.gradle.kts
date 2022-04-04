7679b4e6672e70ad8f12342fc8cf9572259caae1
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2022-04-04 13:49:51 +0100

    HAAR-299: ⬆️ Upgrade to hmpps.gradle-spring-boot (#97)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.0"
-  kotlin("plugin.spring") version "1.6.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.3"
+  kotlin("plugin.spring") version "1.6.20"
 }
 
 configurations {
