2f3d92a90c622dd5579f687845687d849643a378
Joel Stobart (MOJ) <87521440+joelstobart-moj@users.noreply.github.com>
2022-05-09 08:59:56 +0100

    Fix to make detekt not throw an exception (#212)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.5-beta-3"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.6"
   kotlin("plugin.spring") version "1.6.21"
   kotlin("plugin.jpa") version "1.6.21"
-  id("io.gitlab.arturbosch.detekt").version("1.18.0-RC2")
   id("jacoco")
 }
 
