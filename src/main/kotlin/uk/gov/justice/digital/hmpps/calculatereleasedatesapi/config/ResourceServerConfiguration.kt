063fa5e2191e7b233d4ea830c2392d5af3f7fc0d
Joel Stobart (MOJ) <joel.stobart@digital.justice.gov.uk>
2022-06-23 13:46:21 +0100

    Update to the new Security DSL syntax  (should be duplicative)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/config/ResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/config/ResourceServerConfiguration.kt
@@ -1,35 +1,54 @@
 package uk.gov.justice.digital.hmpps.calculatereleasedatesapi.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.config.web.servlet.invoke
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @Configuration
-@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
-class ResourceServerConfiguration : WebSecurityConfigurerAdapter() {
-
-  override fun configure(http: HttpSecurity) {
-    http
-      .sessionManagement()
-      .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-      .and().headers().frameOptions().sameOrigin()
-      .and().csrf().disable()
-      .authorizeRequests { auth ->
-        auth.antMatchers(
-          "/webjars/**",
-          "favicon.ico",
-          "/health/**",
-          "/info",
-          "/swagger-resources/**",
-          "/v3/api-docs/**",
-          "/swagger-ui/**",
-          "/swagger-ui.html",
-          "/h2-console/**"
-        ).permitAll().anyRequest().authenticated()
-      }.oauth2ResourceServer().jwt().jwtAuthenticationConverter(AuthAwareTokenConverter())
+@EnableWebSecurity
+private class ResourceServerConfiguration {
+  @Bean
+  fun web(http: HttpSecurity): SecurityFilterChain {
+    http {
+      sessionManagement {
+        sessionCreationPolicy = SessionCreationPolicy.STATELESS
+      }
+      csrf { disable() }
+      headers {
+        referrerPolicy {
+          policy = ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN
+        }
+      }
+      authorizeHttpRequests {
+        authorize(AntPathRequestMatcher("/webjars/**", HttpMethod.GET.name), permitAll)
+        authorize(AntPathRequestMatcher("favicon.ico", HttpMethod.GET.name), permitAll)
+        authorize(AntPathRequestMatcher("/health/**", HttpMethod.GET.name), permitAll)
+        authorize(AntPathRequestMatcher("/info", HttpMethod.GET.name), permitAll)
+        authorize(AntPathRequestMatcher("/swagger-resources/**", HttpMethod.GET.name), permitAll)
+        authorize(AntPathRequestMatcher("/v3/api-docs/**", HttpMethod.GET.name), permitAll)
+        authorize(AntPathRequestMatcher("/swagger-ui/**", HttpMethod.GET.name), permitAll)
+        authorize(AntPathRequestMatcher("/swagger-ui.html", HttpMethod.GET.name), permitAll)
+        authorize(AntPathRequestMatcher("/h2-console/**", HttpMethod.POST.name), permitAll)
+        authorize(anyRequest, authenticated)
+      }
+      authorizeRequests {
+        authorize(anyRequest, authenticated)
+      }
+      oauth2ResourceServer {
+        jwt {
+          jwtAuthenticationConverter = AuthAwareTokenConverter()
+        }
+      }
+    }
+    return http.build()
   }
 }
