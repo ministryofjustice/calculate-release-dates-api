63594f66f76cc607e31b4fd423231abdaaa4c8ee
Joel Stobart (MOJ) <joel.stobart@digital.justice.gov.uk>
2022-06-23 13:34:52 +0100

    Updates to use Kotlin 17 and Java 18 Updates to a few minor versions Update to the version of Jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.2.2"
-  kotlin("plugin.spring") version "1.6.21"
-  kotlin("plugin.jpa") version "1.6.21"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.3.0-beta"
+  kotlin("plugin.spring") version "1.7.0"
+  kotlin("plugin.jpa") version "1.7.0"
   id("jacoco")
 }
 
@@ -58,18 +58,18 @@ dependencies {
 
   // Database dependencies
   runtimeOnly("org.flywaydb:flyway-core")
-  runtimeOnly("org.postgresql:postgresql:42.3.4")
+  runtimeOnly("org.postgresql:postgresql:42.4.0")
 
   implementation("io.arrow-kt:arrow-core:1.1.2")
   implementation("com.vladmihalcea:hibernate-types-52:2.16.2")
 
   // OpenAPI
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.8")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.8")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.8")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.9")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.9")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.9")
 
-  implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.0.8")
-  implementation("com.amazonaws:aws-java-sdk-sns:1.12.221")
+  implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.1.0")
+  implementation("com.amazonaws:aws-java-sdk-sns:1.12.239")
   implementation("org.springframework.cloud:spring-cloud-aws-messaging:2.2.6.RELEASE")
   implementation("org.springframework:spring-jms")
   implementation("com.google.code.gson:gson:2.9.0")
@@ -80,8 +80,8 @@ dependencies {
   testImplementation("org.awaitility:awaitility-kotlin:4.1.1")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.35.0")
-  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.0.32")
-  testImplementation("org.mockito:mockito-inline:4.5.1")
+  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.0.33")
+  testImplementation("org.mockito:mockito-inline:4.6.1")
   testImplementation("io.projectreactor:reactor-test")
   testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
   testImplementation("com.h2database:h2")
@@ -92,17 +92,17 @@ repositories {
 
 jacoco {
   // You may modify the Jacoco version here
-  toolVersion = "0.8.7"
+  toolVersion = "0.8.8"
 }
 
 kotlin {
   jvmToolchain {
-    (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of("17"))
+    (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of("18"))
   }
 }
 
 java {
-  toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+  toolchain.languageVersion.set(JavaLanguageVersion.of(18))
 }
 
 dependencyCheck {
