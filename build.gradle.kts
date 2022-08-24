b30a4827d22fe885b6d6c65e5c438199d71f354b
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2022-08-24 16:26:53 +0100

    HAAR-714: ⬆️ Upgrade to springboot 2.7.3 via gradle-spring-boot plugin 4.4.2 (#114)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.4.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.4.2"
   kotlin("plugin.spring") version "1.7.10"
 }
 
