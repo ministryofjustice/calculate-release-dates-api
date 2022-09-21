f6ea1eed0657c544b1e1b22b602b5331ab849b70
Luke Harper <luke.harper@digital.justice.gov.uk>
2022-09-21 14:22:14 +0100

    CRS-1046 linting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/SentenceAdjustedCalculationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/SentenceAdjustedCalculationService.kt
@@ -198,8 +198,8 @@ class SentenceAdjustedCalculationService {
       sentenceCalculation.calculatedTotalAddedDays.minus(sentenceCalculation.calculatedTotalDeductedDays)
     val immediateRelease = sentenceCalculation.sentence.sentencedAt == sentenceCalculation.adjustedDeterminateReleaseDate
     if (immediateRelease) {
-      //There may still be adjustments to consider here. If the immediate release occured and then there was a recall,
-      //Any UAL after the recall will need to be added.
+      // There may still be adjustments to consider here. If the immediate release occured and then there was a recall,
+      // Any UAL after the recall will need to be added.
       val adjustedDaysAfterRelease = sentenceCalculation.getTotalAddedDaysAfter(sentenceCalculation.sentence.sentencedAt)
       sentenceCalculation.topUpSupervisionDate = sentenceCalculation.sentence.sentencedAt
         .plusDays(adjustedDaysAfterRelease.toLong())
