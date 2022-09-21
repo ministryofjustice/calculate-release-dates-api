9c0266919a0aa233409e46c292e5b3a4f3aa0e04
Luke Harper <luke.harper@digital.justice.gov.uk>
2022-09-21 14:26:08 +0100

    CRS-1046 adjusting to after sentence date.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/SentenceAdjustedCalculationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/SentenceAdjustedCalculationService.kt
@@ -200,7 +200,7 @@ class SentenceAdjustedCalculationService {
     if (immediateRelease) {
       // There may still be adjustments to consider here. If the immediate release occured and then there was a recall,
       // Any UAL after the recall will need to be added.
-      val adjustedDaysAfterRelease = sentenceCalculation.getTotalAddedDaysAfter(sentenceCalculation.sentence.sentencedAt)
+      val adjustedDaysAfterRelease = sentenceCalculation.getTotalAddedDaysAfter(sentenceCalculation.sentence.sentencedAt.plusDays(1))
       sentenceCalculation.topUpSupervisionDate = sentenceCalculation.sentence.sentencedAt
         .plusDays(adjustedDaysAfterRelease.toLong())
         .plusMonths(TWELVE)
