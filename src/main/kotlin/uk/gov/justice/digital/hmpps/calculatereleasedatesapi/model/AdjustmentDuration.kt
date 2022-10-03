83f3b8b6c6f0e248de1152468e91da85f8342c13
Luke Harper <luke.harper@digital.justice.gov.uk>
2022-10-03 10:35:27 +0100

    CRS-1052 fixing errors.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/AdjustmentDuration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/AdjustmentDuration.kt
@@ -7,6 +7,6 @@ import java.time.temporal.ChronoUnit.DAYS
 data class AdjustmentDuration(
   @Schema(description = "Amount of adjustment")
   val adjustmentValue: Int = 0,
-  @Schema(description = "Unit of adjustment", example = "DAYS")
+  @Schema(description = "Unit of adjustment")
   val type: ChronoUnit = DAYS,
 )
