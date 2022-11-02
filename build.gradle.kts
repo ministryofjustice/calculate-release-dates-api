5ec2c8908a6bb684a4a94fbddfebb93a87d2b7fb
petergphillips <peter@greenthistle.co.uk>
2022-11-02 10:30:07 +0000

    SDI-438: ⬆️  Upgrade to latest for spring security 5.7.5 (#123)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.6"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.7"
   kotlin("plugin.spring") version "1.7.20"
 }
 
