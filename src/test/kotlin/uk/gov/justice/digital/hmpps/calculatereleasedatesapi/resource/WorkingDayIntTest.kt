a1d35dcb52802568614cf7869577635570ad0c78
Luke Harper <luke.harper@digital.justice.gov.uk>
2023-01-09 10:13:16 +0000

    CRS-1153 removing spring cache from bank holiday.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/resource/WorkingDayIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/resource/WorkingDayIntTest.kt
@@ -4,20 +4,12 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.cache.CacheManager
 import org.springframework.http.MediaType
-import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.config.CacheConfiguration.Companion.BANK_HOLIDAYS_CACHE_NAME
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.WorkingDay
-import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.external.BankHolidays
 import java.time.LocalDate
-import java.util.concurrent.ConcurrentHashMap
 
 class WorkingDayIntTest : IntegrationTestBase() {
-
-  @Autowired
-  lateinit var cacheManager: CacheManager
 
   @Test
   fun `nextWorkingDay test weekend adjustment`() {
@@ -29,17 +21,12 @@ class WorkingDayIntTest : IntegrationTestBase() {
   }
 
   @Test
-  fun `previousWorkingDay test weekend adjustment and check that the cache is populated`() {
+  fun `previousWorkingDay test weekend adjustment`() {
     val result = makeApiCall("/working-day/previous/$SATURDAY")
-    @Suppress("UNCHECKED_CAST") val nativeCache: ConcurrentHashMap<Any, Any> =
-      cacheManager.getCache(BANK_HOLIDAYS_CACHE_NAME)!!.nativeCache as ConcurrentHashMap<Any, Any>
-    val holidays: BankHolidays = nativeCache.values.first() as BankHolidays
 
     assertEquals(FRIDAY, result.date)
     assertTrue(result.adjustedForWeekend)
     assertFalse(result.adjustedForBankHoliday)
-    assertEquals(1, nativeCache.size)
-    assertTrue(holidays.englandAndWales.events.isNotEmpty())
   }
 
   private fun makeApiCall(uri: String) = webTestClient.get()
