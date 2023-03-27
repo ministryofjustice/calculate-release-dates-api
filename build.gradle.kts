fa1e29615e86da4711ce96ea0f77a8714be93740
Joel Stobart (MOJ) <joel.stobart@digital.justice.gov.uk>
2023-03-27 12:04:02 +0100

    Update the gradle build dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,30 +33,30 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-actuator")
 
   // GOVUK Notify:
-  implementation("uk.gov.service.notify:notifications-java-client:3.19.0-RELEASE")
+  implementation("uk.gov.service.notify:notifications-java-client:3.19.1-RELEASE")
 
   // Enable kotlin reflect
   implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.20")
 
   // Three Ten Date Calculations
-  implementation("org.threeten:threeten-extra:1.7.1")
+  implementation("org.threeten:threeten-extra:1.7.2")
 
   // Database dependencies
   runtimeOnly("org.flywaydb:flyway-core")
   runtimeOnly("org.postgresql:postgresql:42.5.1")
 
   implementation("io.arrow-kt:arrow-core:1.1.2")
-  implementation("com.vladmihalcea:hibernate-types-52:2.20.0")
+  implementation("com.vladmihalcea:hibernate-types-52:2.21.1")
 
   // OpenAPI
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.13")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.13")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.13")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.15")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.15")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.15")
 
-  implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.1.0")
+  implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.1.2")
   implementation("org.springframework.cloud:spring-cloud-aws-messaging:2.2.6.RELEASE")
-  implementation("org.springframework:spring-jms:5.3.22")
-  implementation("com.google.code.gson:gson:2.10")
+  implementation("org.springframework:spring-jms:5.3.26")
+  implementation("com.google.code.gson:gson:2.10.1")
 
   // SQS
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.2.0")
@@ -76,10 +76,10 @@ dependencies {
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.36.0")
-  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.1.8")
-  testImplementation("org.mockito:mockito-inline:4.8.0")
-  testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.36.1")
+  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.1.12")
+  testImplementation("org.mockito:mockito-inline:5.2.0")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
   testImplementation("com.h2database:h2")
 
   if (project.hasProperty("docs")) {
@@ -108,7 +108,7 @@ java {
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-      jvmTarget = "18"
+      jvmTarget = "19"
     }
   }
 }
