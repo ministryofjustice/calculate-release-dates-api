d11a71168489b58e501fa56985c4d8903aa10d6d
Luke Harper <luke.harper@digital.justice.gov.uk>
2023-04-12 15:53:53 +0100

    MDWFP-111 fixing bug where there is a recall.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/RelevantRemandService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/RelevantRemandService.kt
@@ -10,6 +10,8 @@ import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.RelevantReman
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.external.PrisonApiSourceData
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.external.SentenceAdjustment
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.external.SentenceAdjustmentType
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.external.SentenceAndOffences
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.external.SentenceCalculationType
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.util.isBeforeOrEqualTo
 
 @Service
@@ -26,19 +28,39 @@ class RelevantRemandService(
 
     val booking = bookingService.getBooking(sourceData, calculationUserInputs)
     val calculationResult = calculationService.calculateReleaseDates(booking).second
-    return RelevantRemandCalculationResult(if (calculationResult.dates[ReleaseDateType.CRD] != null) calculationResult.dates[ReleaseDateType.CRD]!! else calculationResult.dates[ReleaseDateType.ARD]!!)
+    return RelevantRemandCalculationResult(
+      if (calculationResult.dates[ReleaseDateType.CRD] != null) {calculationResult.dates[ReleaseDateType.CRD]!!}
+      else if (calculationResult.dates[ReleaseDateType.ARD] != null) {calculationResult.dates[ReleaseDateType.ARD]!!}
+      else {calculationResult.dates[ReleaseDateType.PRRD]!!}
+    )
   }
 
   private fun filterSentencesAndAdjustmentsForRelevantRemandCalc(sourceData: PrisonApiSourceData, request: RelevantRemandCalculationRequest): PrisonApiSourceData {
     return sourceData.copy(
       sentenceAndOffences = sourceData.sentenceAndOffences.filter { it.sentenceDate.isBeforeOrEqualTo(request.sentenceDate) },
       bookingAndSentenceAdjustments = sourceData.bookingAndSentenceAdjustments.copy(
-        sentenceAdjustments = sourceData.bookingAndSentenceAdjustments.sentenceAdjustments.filter { it.type != SentenceAdjustmentType.REMAND } +
-          request.relevantRemands.map { SentenceAdjustment(it.sentenceSequence, true, it.from, it.to, it.days, SentenceAdjustmentType.REMAND) }
+        sentenceAdjustments = sourceData.bookingAndSentenceAdjustments.sentenceAdjustments.filter { !listOf(SentenceAdjustmentType.REMAND, SentenceAdjustmentType.RECALL_SENTENCE_REMAND, SentenceAdjustmentType.UNUSED_REMAND).contains(it.type) } +
+          request.relevantRemands.map {
+            val sentence = findSentence(sourceData.sentenceAndOffences, it.sentenceSequence)
+            val adjustmentType: SentenceAdjustmentType = if (sentence != null) {
+              val sentenceType = SentenceCalculationType.from(sentence.sentenceCalculationType)
+              if (sentenceType.recallType != null) {
+                SentenceAdjustmentType.RECALL_SENTENCE_REMAND
+              } else {
+                SentenceAdjustmentType.REMAND
+              }
+            } else {
+              SentenceAdjustmentType.REMAND
+            }
+            SentenceAdjustment(it.sentenceSequence, true, it.from, it.to, it.days, adjustmentType)
+          }
       )
     )
   }
 
+  private fun findSentence(sentenceAndOffences: List<SentenceAndOffences>, sentenceSequence: Int): SentenceAndOffences? {
+    return sentenceAndOffences.find { it.sentenceSequence == sentenceSequence }
+  }
   companion object {
     val log: Logger = LoggerFactory.getLogger(this::class.java)
   }
