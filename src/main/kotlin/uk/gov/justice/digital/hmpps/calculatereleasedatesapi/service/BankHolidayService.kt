76048a7b8e358e8c232ab1eb02c00db08abf0a15
Joel Stobart (MOJ) <joel.stobart@digital.justice.gov.uk>
2023-05-15 08:57:22 +0100

    Small linting fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/BankHolidayService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/BankHolidayService.kt
@@ -54,7 +54,7 @@ class BankHolidayService(
       bankHolidayCacheRepository.save(cache)
       bankHolidays
     } else {
-      objectMapper.convertValue(cached.data,BankHolidays::class.java)
+      objectMapper.convertValue(cached.data, BankHolidays::class.java)
     }
   }
 
