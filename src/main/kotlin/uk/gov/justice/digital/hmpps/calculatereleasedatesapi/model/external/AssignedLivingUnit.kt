350efaeedd6e2f209851cf03aecba5477fc4179c
Jamie Bowdidge <jamie.bowdige@digital.justice.gov.uk>
2023-08-11 11:48:19 +0100

    CRS-1536 store the living unit details with the prisoner details at the time of calculation

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/external/AssignedLivingUnit.kt
@@ -0,0 +1,8 @@
+package uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.external
+
+data class AssignedLivingUnit(
+  val agencyId: String = "",
+  val locationId: Number = 0,
+  val description: String = "",
+  val agencyName: String = "",
+)
