e59895542bc84dff5431c8809f2206817ff3c6a0
petergphillips <peter@greenthistle.co.uk>
2023-09-01 14:52:33 +0100

    SDIT-1060: ⬆️ Upgrade to java 20 (#169)

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,13 +12,13 @@ dependencies {
 }
 
 java {
-  toolchain.languageVersion.set(JavaLanguageVersion.of(19))
+  toolchain.languageVersion.set(JavaLanguageVersion.of(20))
 }
 
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-      jvmTarget = "19"
+      jvmTarget = "20"
     }
   }
 }
