8d91e3e35c3c7eeb7c6804614c1f94ba4566c1fc
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2023-10-05 14:09:11 +0100

    lint fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/resource/NonFridayReleaseController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/resource/NonFridayReleaseController.kt
@@ -10,7 +10,6 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.MediaType
-import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
