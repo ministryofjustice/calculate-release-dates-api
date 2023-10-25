0946bb6c9809cb19def3bed6df16a6d02236d1f9
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2023-10-25 09:00:59 +0100

    CRS-1667 return missing data for manual endpoints

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/ManualComparisonService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/ManualComparisonService.kt
@@ -6,6 +6,8 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.entity.Comparison
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.ComparisonOverview
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.ComparisonSummary
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.ManualComparisonInput
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.repository.ComparisonPersonRepository
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.repository.ComparisonRepository
@@ -29,8 +31,8 @@ class ManualComparisonService(
     return initialComparisonCreated
   }
 
-  fun listManual(): List<Comparison> {
-    return comparisonRepository.findAllByManualInput(true)
+  fun listManual(): List<ComparisonSummary> {
+    return comparisonRepository.findAllByManualInput(true).map { transform(it) }
   }
 
   fun getCountOfPersonsInComparisonByComparisonReference(shortReference: String): Long {
@@ -39,8 +41,10 @@ class ManualComparisonService(
     } ?: 0
   }
 
-  fun getComparisonByComparisonReference(comparisonReference: String): Comparison {
-    return comparisonRepository.findByManualInputAndComparisonShortReference(true, comparisonReference) ?: throw EntityNotFoundException("No comparison results exist for comparisonReference $comparisonReference ")
+  fun getComparisonByComparisonReference(comparisonReference: String): ComparisonOverview {
+    val comparison = comparisonRepository.findByManualInputAndComparisonShortReference(true, comparisonReference) ?: throw EntityNotFoundException("No comparison results exist for comparisonReference $comparisonReference ")
+    val mismatches = comparisonPersonRepository.findByComparisonIdIs(comparison.id)
+    return transform(comparison, mismatches)
   }
 
   companion object {
