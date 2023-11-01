9315acde4988b4134bfbdf796aa6e315946c3d92
Luke Harper <luke.harper@digital.justice.gov.uk>
2023-11-01 17:37:07 +0000

    ADJUST1-248-correction-to-no-deductions

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/unuseddeductions/service/UnusedDeductionsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/unuseddeductions/service/UnusedDeductionsService.kt
@@ -21,8 +21,13 @@ class UnusedDeductionsService(
     val deductions = adjustments
       .filter { it.adjustmentType === AdjustmentServiceAdjustmentType.REMAND || it.adjustmentType === AdjustmentServiceAdjustmentType.TAGGED_BAIL }
 
-    val allDeductionsEnteredInDps =
-      deductions.isNotEmpty() && deductions.all { it.days != null || it.daysBetween != null }
+    if (deductions.isEmpty()) {
+      setUnusedDeductions(0, adjustments, deductions)
+      setEffectiveDays(0, adjustments)
+      return
+    }
+
+    val allDeductionsEnteredInDps = deductions.all { it.days != null || it.daysBetween != null }
 
     if (allDeductionsEnteredInDps) {
       val unusedDeductions =
