fc52812be8c9ac61cf96ed800f13b4819ca91afe
jbowdidge-moj <122450608+jbowdidge-moj@users.noreply.github.com>
2023-11-10 10:50:13 +0000

    CRS-1683 Add calculation type to historic calculation if available (#492)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/HistoricCalculation.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/HistoricCalculation.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model
 
 import com.fasterxml.jackson.annotation.JsonFormat
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.entity.CalculationType
 import java.time.LocalDateTime
 
 data class HistoricCalculation(
@@ -9,4 +10,5 @@ data class HistoricCalculation(
   val calculationSource: CalculationSource,
   val calculationViewConfiguration: CalculationViewConfiguration?,
   val commentText: String,
+  val calculationType: CalculationType?,
 )
