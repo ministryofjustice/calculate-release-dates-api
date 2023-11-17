0f9277a3e5104c65aa9ec3c499216d4d2b34b080
Luke Harper <luke.harper@digital.justice.gov.uk>
2023-11-17 13:10:39 +0000

    ADJUST1-290 Remove unused deductions adjustments code.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/unuseddeductions/controller/UnusedDeductionsController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/unuseddeductions/controller/UnusedDeductionsController.kt
@@ -18,13 +18,11 @@ import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.external.AdjustmentServiceAdjustment
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.external.UnusedDeductionCalculationResponse
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.unuseddeductions.service.UnusedDeductionsCalculationService
-import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.unuseddeductions.service.UnusedDeductionsService
 
 @RestController
 @RequestMapping("/unused-deductions", produces = [MediaType.APPLICATION_JSON_VALUE])
 @Tag(name = "unused-deductions-controller", description = "Operations involving a calculating unused deductions")
 class UnusedDeductionsController(
-  val unusedDeductionsService: UnusedDeductionsService,
   val unusedDeductionsCalculationService: UnusedDeductionsCalculationService,
 ) {
 
@@ -52,27 +50,6 @@ class UnusedDeductionsController(
     return UnusedDeductionCalculationResponse(unusedDeductionsCalculationService.calculate(adjustments, prisonerId))
   }
 
-  @PostMapping(value = ["/{prisonerId}"])
-  @PreAuthorize("hasAnyRole('SYSTEM_USER', 'RELEASE_DATES_CALCULATOR')")
-  @ResponseBody
-  @Operation(
-    summary = "Calculate unused deductions and push the data back into adjustments api.",
-  )
-  @ApiResponses(
-    value = [
-      ApiResponse(responseCode = "200", description = "Calculated"),
-      ApiResponse(responseCode = "401", description = "Unauthorised, requires a valid Oauth2 token"),
-      ApiResponse(responseCode = "403", description = "Forbidden, requires an appropriate role"),
-    ],
-  )
-  fun handleUnusedDeductionsUpdate(
-    @Parameter(required = true, example = "A1234AB", description = "The prisoners ID (aka nomsId)")
-    @PathVariable("prisonerId")
-    prisonerId: String,
-  ) {
-    log.info("Request received to handle unused deduction change for $prisonerId")
-    unusedDeductionsService.handleUnusedDeductionRequest(prisonerId)
-  }
   companion object {
     val log: Logger = LoggerFactory.getLogger(this::class.java)
   }
