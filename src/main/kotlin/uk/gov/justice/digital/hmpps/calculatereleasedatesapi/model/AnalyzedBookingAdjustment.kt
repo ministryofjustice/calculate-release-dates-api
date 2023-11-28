2a67eb58a3fcd5723cd7cf650e15500694acc787
jbowdidge-moj <122450608+jbowdidge-moj@users.noreply.github.com>
2023-11-28 15:35:48 +0000

    CRS-1699 identify new adjustments since last calculation (#508)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/AnalyzedBookingAdjustment.kt
@@ -0,0 +1,13 @@
+package uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model
+
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.external.BookingAdjustmentType
+import java.time.LocalDate
+
+data class AnalyzedBookingAdjustment(
+  val active: Boolean,
+  val fromDate: LocalDate,
+  val toDate: LocalDate? = null,
+  val numberOfDays: Int,
+  val type: BookingAdjustmentType,
+  val analysisResult: AdjustmentAnalysisResult,
+)
