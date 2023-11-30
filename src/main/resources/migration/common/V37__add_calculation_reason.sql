ff96bf3d2f44a9684d05f44996937f62ebe2680e
dave-virden <151018159+dave-virden@users.noreply.github.com>
2023-11-30 12:55:49 +0000

    Further changes from PR 508 * Removed rand and replace with is_active. Order will be determined by id initially. * Id is now generated.

--- src/main/resources/migration/common/V37__add_calculation_reason.sql
+++ src/main/resources/migration/common/V37__add_calculation_reason.sql
@@ -1,9 +1,10 @@
 CREATE TABLE calculation_reason
 (
-    id           INTEGER PRIMARY KEY,
-    rank         INTEGER NOT NULL, -- Determines the order on the page, less than zero will not be displayed.
-    display_name VARCHAR(40),
-    is_other     BOOLEAN DEFAULT FALSE
+    id           SERIAL
+        CONSTRAINT calculation_reason_pk PRIMARY KEY,
+    is_active    BOOLEAN NOT NULL DEFAULT TRUE,
+    is_other     BOOLEAN NOT NULL DEFAULT FALSE,
+    display_name VARCHAR(40)
 );
 
 ALTER TABLE calculation_request
@@ -11,22 +12,26 @@ ALTER TABLE calculation_request
 ALTER TABLE calculation_request
     ADD COLUMN other_reason_for_calculation VARCHAR(40);
 
+INSERT INTO calculation_reason (display_name)
+VALUES ('Initial calculation'),
+       ('Transfer check'),
+       ('Lodged warrant'),
+       ('ADA - Additional Days Awarded'),
+       ('RADA - Restoration of Added Days Awarded'),
+       ('Recording a non-calculated date'),
+       ('Further sentences and terms'),
+       ('14 day check'),
+       ('2 day check'),
+       ('Appeal decision');
+
+INSERT INTO calculation_reason (display_name, is_other)
+VALUES ('Other', true);
+
+INSERT INTO calculation_reason (display_name, is_active)
+VALUES ('Reason for calculation not provided', false);
+
 UPDATE calculation_request
-SET reason_for_calculation = -1
+SET reason_for_calculation = (SELECT id
+                              FROM calculation_reason
+                              WHERE display_name = 'Reason for calculation not provided')
 WHERE reason_for_calculation IS NULL;
-
-INSERT INTO calculation_reason (id, rank, display_name)
-VALUES (1, 10, 'Initial calculation'),
-       (2, 20, 'Transfer check'),
-       (3, 30, 'Lodged warrant'),
-       (4, 40, 'ADA - Additional Days Awarded'),
-       (5, 50, 'RADA - Restoration of Added Days Awarded'),
-       (6, 60, 'Recording a non-calculated date'),
-       (7, 70, 'Further sentences and terms'),
-       (8, 80, '14 day check'),
-       (9, 90, '2 day check'),
-       (10, 100, 'Appeal decision'),
-       (-1, -1, 'Reason for calculation not provided');
-
-INSERT INTO calculation_reason (id, rank, display_name, is_other)
-VALUES (11, 110, 'Other', true);
