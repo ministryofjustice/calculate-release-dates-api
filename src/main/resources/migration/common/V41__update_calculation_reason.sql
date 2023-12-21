ce712b5cf87c37551b11a923ae0a044aefab1d08
dave-virden <151018159+dave-virden@users.noreply.github.com>
2023-12-21 16:29:29 +0000

    Not Null constraint not appropriate until reason is mandatory.

--- src/main/resources/migration/common/V41__update_calculation_reason.sql
+++ src/main/resources/migration/common/V41__update_calculation_reason.sql
@@ -1,7 +1,4 @@
 ALTER TABLE calculation_reason ADD COLUMN is_bulk BOOLEAN NOT NULL DEFAULT FALSE;
 
-ALTER TABLE calculation_request
-    ALTER COLUMN reason_for_calculation SET NOT NULL;
-
 INSERT INTO calculation_reason (display_name, is_active, is_bulk)
 VALUES ('Bulk Comparison', false, true);
