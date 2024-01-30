a1e82e9759da0ab9f3465de664190a97f4854c42
Justin Gardiner <justin.gardiner@digital.justice.gov.uk>
2024-01-30 10:25:38 +0000

    CRS-1751: Add validation to CreateComparisonDiscrepancyRequest. Rename INELIGIBLE_FOR_HDCED to INELIGIBLE_FOR_HDC.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/CreateComparisonDiscrepancyRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/CreateComparisonDiscrepancyRequest.kt
@@ -1,23 +1,29 @@
 package uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model
 
 import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.NotNull
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.enumerations.DiscrepancyImpact
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.enumerations.DiscrepancyPriority
 
 data class CreateComparisonDiscrepancyRequest(
 
   @Schema(description = "The impact of the discrepancy")
+  @field:NotNull
   val impact: DiscrepancyImpact,
 
   @Schema(description = "The causes for the mismatch")
+  @field:NotNull
   val causes: List<DiscrepancyCause>,
 
   @Schema(description = "Any extra detail about the discrepancy")
+  @field:NotNull
   val detail: String?,
 
   @Schema(description = "The priority of resolving the discrepancy")
+  @field:NotNull
   val priority: DiscrepancyPriority,
 
   @Schema(description = "The recommended action that needs to be taken for this discrepancy")
+  @field:NotNull
   val action: String,
 )
