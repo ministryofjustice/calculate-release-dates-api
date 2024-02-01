73c05451ab971c05ee4def58bfbf6066d3910395
carloveo-moj <144013256+carloveo-moj@users.noreply.github.com>
2024-02-01 13:38:46 +0000

    CRS-1803 corrected logging in exception handler (#193)
    * CRS-1803 corrected logging in exception handler
    
    * correct method name

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstemplatepackagename/config/HmppsTemplateKotlinExceptionHandler.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstemplatepackagename/config/HmppsTemplateKotlinExceptionHandler.kt
@@ -14,7 +14,7 @@ import org.springframework.web.servlet.resource.NoResourceFoundException
 @RestControllerAdvice
 class HmppsTemplateKotlinExceptionHandler {
   @ExceptionHandler(ValidationException::class)
-  fun handleValidationException(e: Exception): ResponseEntity<ErrorResponse> = ResponseEntity
+  fun handleValidationException(e: ValidationException): ResponseEntity<ErrorResponse> = ResponseEntity
     .status(BAD_REQUEST)
     .body(
       ErrorResponse(
@@ -25,15 +25,15 @@ class HmppsTemplateKotlinExceptionHandler {
     ).also { log.info("Validation exception: {}", e.message) }
 
   @ExceptionHandler(NoResourceFoundException::class)
-  fun handleValidationException(e: NoResourceFoundException): ResponseEntity<ErrorResponse> = ResponseEntity
+  fun handleNoResourceFoundException(e: NoResourceFoundException): ResponseEntity<ErrorResponse> = ResponseEntity
     .status(NOT_FOUND)
     .body(
       ErrorResponse(
         status = NOT_FOUND,
-        userMessage = "Validation failure: ${e.message}",
+        userMessage = "No resource found failure: ${e.message}",
         developerMessage = e.message,
       ),
-    ).also { log.info("Validation exception: {}", e.message) }
+    ).also { log.info("No resource found exception: {}", e.message) }
 
   @ExceptionHandler(Exception::class)
   fun handleException(e: Exception): ResponseEntity<ErrorResponse> = ResponseEntity
