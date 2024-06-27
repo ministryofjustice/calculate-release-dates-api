2e54a9400585899025ee23fd599a626f4b82f25d
FaizJD030 <165897256+FaizJD030@users.noreply.github.com>
2024-06-27 12:11:30 +0100

    CRS-1993 Update a Calc reason from HDC to HDCAD (#726)

---
+++ src/main/resources/migration/common/V54__amend_calcReason_HDC_to_HDCAD.sql
@@ -0,0 +1,3 @@
+UPDATE calculation_reason
+SET display_name = 'Recording a non-calculated date (including HDCAD, APD or ROTL)'
+WHERE display_name = 'Recording a non-calculated date (including HDC, APD or ROTL)';
