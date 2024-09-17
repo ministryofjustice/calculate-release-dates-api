6723478b3fae97884747e174c36252c42f864577
Joel Stobart <>
2024-09-17 12:55:01 +0100

    introduce new breach of the 97 act error when the offence occurs after 1997

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/validation/ValidationCode.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/validation/ValidationCode.kt
@@ -79,5 +79,10 @@ enum class ValidationCode(val message: String, val validationType: ValidationTyp
       "For example, ‘Encouraging/Assisting’ a Rape SX03001 would be SX03001E.",
     UNSUPPORTED_OFFENCE,
   ),
+  UNSUPPORTED_BREACH_97(
+    "Any offences that include the inchoate ‘Encouraging/ Assisting’ should be recorded as the underlying act, ending in the letter ‘e’.\n" +
+      "For example, ‘Encouraging/Assisting’ a Rape SX03001 would be SX03001E.",
+    UNSUPPORTED_OFFENCE,
+  ),
   UNSUPPORTED_SUSPENDED_OFFENCE("Replace this offence in NOMIS with the original offence they were sentenced for, then reload this page.", SUSPENDED_OFFENCE),
 }
