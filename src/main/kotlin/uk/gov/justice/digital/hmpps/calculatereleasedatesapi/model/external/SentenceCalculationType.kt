62b3dd0a8c14ccf7a3cda8670fcf4801bc05d0e0
srowlands-moj <145464259+srowlands-moj@users.noreply.github.com>
2024-10-30 11:11:26 +0000

    CRS-2185 Add STS21 type to SentenceCalc types and make it unsupported (#911)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/external/SentenceCalculationType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/external/SentenceCalculationType.kt
@@ -116,6 +116,7 @@ enum class SentenceCalculationType(
   SEC91(isSupported = false, isIndeterminate = false),
   VOO(isSupported = false, isIndeterminate = false),
   TISCS(isSupported = false, isIndeterminate = false),
+  STS21(isSupported = false, isIndeterminate = false),
   ;
 
   companion object {
