1cdfceda30393a8903aac4bda664babe34e80e47
matthew.green <matthew.green3@digital.justice.gov.uk>
2024-11-12 11:46:25 +0000

    CRS-2186-shpo-offence-violation
    validation to prevent release dates being calculated for prisoners where they have at least one SDS40 eligible SDS sentence imposed for a Sentencing Act 2020 (SA20) Breach of Sexual Harm Prevention (SHPO) order offence and the offence date is before 1/12/20.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/validation/SHPOValidationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/validation/SHPOValidationService.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.calculatereleasedatesapi.validation
 
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.Booking
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.CalculableSentence
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.ConsecutiveSentence
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.StandardDeterminateSentence
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.service.ImportantDates.SDS_40_COMMENCEMENT_DATE
@@ -12,28 +13,55 @@ class SHPOValidationService {
 
   private val shpoOffenceCodes = listOf("SE20005", "SE20006", "SE20012", "SE20012A")
 
-  internal fun validate(booking: Booking): List<ValidationMessage> = booking.getAllExtractableSentences()
-    .flatMap { if (it is ConsecutiveSentence) it.orderedSentences else listOf(it) }
-    .filterIsInstance<StandardDeterminateSentence>()
-    .filter { isShpoViolation(it) }
-    .map {
-      ValidationMessage(
-        ValidationCode.SHPO_INVALID_DATE,
-        listOf(it.caseSequence.toString(), it.lineSequence.toString()),
-      )
+  internal fun validate(booking: Booking): List<ValidationMessage> = booking.getAllExtractableSentences().flatMap {
+    when (it) {
+      is ConsecutiveSentence -> validateConsecutiveSentence(it)
+      is StandardDeterminateSentence -> validateStandardDeterminateSentence(it)
+      else -> emptyList()
+    }
+  }
+
+  private fun validateConsecutiveSentence(consecutiveSentence: ConsecutiveSentence): List<ValidationMessage> {
+    if (consecutiveSentence.sentencedAt <= SDS_40_COMMENCEMENT_DATE &&
+      !isConsecutiveSdsHistoricSentenceInValid(consecutiveSentence)
+    ) {
+      return emptyList()
     }
 
-  private fun isShpoViolation(sentence: StandardDeterminateSentence): Boolean {
-    if (!isRelevantOffence(sentence)) return false
-    if (isSdsSentenceInValid(sentence)) return true
-    if (isSdsHistoricSentenceInValid(sentence)) return true
-    return false
+    return consecutiveSentence.orderedSentences
+      .filterIsInstance<StandardDeterminateSentence>()
+      .filter { isRelevantOffence(it) && it.offence.committedAt < SHPO_OFFENCE_COMMENCEMENT_DATE }
+      .map {
+        ValidationMessage(
+          ValidationCode.SHPO_INVALID_DATE,
+          listOf(it.caseSequence.toString(), it.lineSequence.toString()),
+        )
+      }
   }
+
+  private fun validateStandardDeterminateSentence(sentence: StandardDeterminateSentence): List<ValidationMessage> =
+    if (isShpoViolation(sentence)) {
+      listOf(
+        ValidationMessage(
+          ValidationCode.SHPO_INVALID_DATE,
+          listOf(sentence.caseSequence.toString(), sentence.lineSequence.toString()),
+        ),
+      )
+    } else {
+      emptyList()
+    }
+
+  private fun isShpoViolation(sentence: StandardDeterminateSentence): Boolean = isRelevantOffence(sentence) &&
+    (isSdsSentenceInValid(sentence) || isSdsHistoricSentenceInValid(sentence))
 
   private fun isRelevantOffence(sentence: StandardDeterminateSentence): Boolean =
     !sentence.isSDSPlus && shpoOffenceCodes.contains(sentence.offence.offenceCode)
 
-  private fun isSdsSentenceInValid(sentence: StandardDeterminateSentence): Boolean =
+  private fun isConsecutiveSdsHistoricSentenceInValid(consecutiveSentence: ConsecutiveSentence): Boolean =
+    consecutiveSentence.sentencedAt.isBefore(SDS_40_COMMENCEMENT_DATE) &&
+      consecutiveSentence.sentenceCalculation.releaseDate.isAfter(SDS_40_COMMENCEMENT_DATE)
+
+  private fun isSdsSentenceInValid(sentence: CalculableSentence): Boolean =
     sentence.sentencedAt >= SDS_40_COMMENCEMENT_DATE &&
       sentence.offence.committedAt < SHPO_OFFENCE_COMMENCEMENT_DATE
 
