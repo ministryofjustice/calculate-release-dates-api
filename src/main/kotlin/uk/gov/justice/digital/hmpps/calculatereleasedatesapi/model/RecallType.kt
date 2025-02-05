de53ad8333b36d74773736ff04f60154d6bb3955
Joel Stobart <>
2025-02-05 14:13:45 +0000

    Reflecting Lukes changes, and also incorporating a new Section 255 recall type (HDC)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/RecallType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/RecallType.kt
@@ -4,6 +4,7 @@ enum class RecallType(
   val isFixedTermRecall: Boolean = false,
 ) {
   STANDARD_RECALL(),
+  STANDARD_RECALL_255(),
   FIXED_TERM_RECALL_14(14, true),
   FIXED_TERM_RECALL_28(28, true),
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/external/SentenceCalculationType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/external/SentenceCalculationType.kt
@@ -10,6 +10,7 @@ import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.RecallType
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.RecallType.FIXED_TERM_RECALL_14
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.RecallType.FIXED_TERM_RECALL_28
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.RecallType.STANDARD_RECALL
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.RecallType.STANDARD_RECALL_255
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.SopcSentence
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.StandardDeterminateSentence
 
@@ -17,53 +18,52 @@ private val UNSUPPORTED: Class<out AbstractSentence>? = null
 
 // These SentenceCalculationType values come from NOMIS - they map to offender_sentences.sentence_calc_type in NOMIS
 enum class SentenceCalculationType(
-  val recallType: RecallType? = null,
   val sentenceClazz: Class<out AbstractSentence>? = StandardDeterminateSentence::class.java,
+  val recallType: RecallType? = null,
   val primaryName: String? = null,
   val isSDS40Eligible: Boolean = false,
   val toreraEligibilityType: ToreraEligibilityType = ToreraEligibilityType.NONE,
   val sdsPlusEligibilityType: SDSPlusEligibilityType = SDSPlusEligibilityType.NONE,
 ) {
+  //region SDS / ORA Sentences
   ADIMP(isSDS40Eligible = true, toreraEligibilityType = ToreraEligibilityType.SDS, sdsPlusEligibilityType = SDSPlusEligibilityType.SDS),
   ADIMP_ORA(isSDS40Eligible = true, toreraEligibilityType = ToreraEligibilityType.SDS, sdsPlusEligibilityType = SDSPlusEligibilityType.SDS),
   YOI(isSDS40Eligible = true, toreraEligibilityType = ToreraEligibilityType.SDS, sdsPlusEligibilityType = SDSPlusEligibilityType.SDS),
   YOI_ORA(isSDS40Eligible = true, toreraEligibilityType = ToreraEligibilityType.SDS, sdsPlusEligibilityType = SDSPlusEligibilityType.SDS),
-  SEC91_03(isSDS40Eligible = true),
-  SEC91_03_ORA(isSDS40Eligible = true),
   SEC250(isSDS40Eligible = true, toreraEligibilityType = ToreraEligibilityType.SDS, sdsPlusEligibilityType = SDSPlusEligibilityType.SECTION250),
   SEC250_ORA(isSDS40Eligible = true, toreraEligibilityType = ToreraEligibilityType.SDS, sdsPlusEligibilityType = SDSPlusEligibilityType.SECTION250),
-  LR(recallType = STANDARD_RECALL, isSDS40Eligible = true),
-  LR_ORA(recallType = STANDARD_RECALL, isSDS40Eligible = true),
-  LR_YOI_ORA(recallType = STANDARD_RECALL, isSDS40Eligible = true),
-  LR_SEC91_ORA(recallType = STANDARD_RECALL, isSDS40Eligible = true),
-  LRSEC250_ORA(recallType = STANDARD_RECALL, isSDS40Eligible = true),
-  FTR_14_ORA(recallType = FIXED_TERM_RECALL_14, primaryName = "14FTR_ORA", isSDS40Eligible = true),
-  FTR_14_HDC_ORA(
-    recallType = FIXED_TERM_RECALL_14,
-    primaryName = "14FTRHDC_ORA",
-    sentenceClazz = UNSUPPORTED,
-    isSDS40Eligible = true,
-  ),
-  FTR(recallType = FIXED_TERM_RECALL_28, isSDS40Eligible = true),
-  FTR_ORA(recallType = FIXED_TERM_RECALL_28, isSDS40Eligible = true),
-  FTR_HDC_ORA(recallType = FIXED_TERM_RECALL_28, sentenceClazz = UNSUPPORTED, isSDS40Eligible = true),
-  FTR_SCH15(recallType = FIXED_TERM_RECALL_28, isSDS40Eligible = true),
-  FTRSCH15_ORA(recallType = FIXED_TERM_RECALL_28, isSDS40Eligible = true),
-  FTRSCH18(recallType = FIXED_TERM_RECALL_28, isSDS40Eligible = true),
-  FTRSCH18_ORA(recallType = FIXED_TERM_RECALL_28, isSDS40Eligible = true),
-  FTR_HDC(sentenceClazz = UNSUPPORTED, isSDS40Eligible = true),
-  HDR(sentenceClazz = UNSUPPORTED, isSDS40Eligible = true),
-  HDR_ORA(sentenceClazz = UNSUPPORTED, isSDS40Eligible = true, recallType = STANDARD_RECALL),
+  SEC91_03(isSDS40Eligible = true),
+  SEC91_03_ORA(isSDS40Eligible = true),
+  //endregion
+
+  //region Extended Determinate Sentences
   LASPO_AR(sentenceClazz = ExtendedDeterminateSentence::class.java),
   LASPO_DR(sentenceClazz = ExtendedDeterminateSentence::class.java),
   EDS18(sentenceClazz = ExtendedDeterminateSentence::class.java),
   EDS21(sentenceClazz = ExtendedDeterminateSentence::class.java),
   EDSU18(sentenceClazz = ExtendedDeterminateSentence::class.java),
+  //endregion
+
+  //region SOPC Sentences
   SDOPCU18(sentenceClazz = SopcSentence::class.java),
   SOPC18(sentenceClazz = SopcSentence::class.java, toreraEligibilityType = ToreraEligibilityType.SOPC),
   SOPC21(sentenceClazz = SopcSentence::class.java, toreraEligibilityType = ToreraEligibilityType.SOPC),
   SEC236A(sentenceClazz = SopcSentence::class.java, toreraEligibilityType = ToreraEligibilityType.SOPC),
+  //endregion
+
+  //region Fine, BOTUS and DTO Sentences
   AFINE(sentenceClazz = AFineSentence::class.java, primaryName = "A/FINE"),
+  DTO(sentenceClazz = DetentionAndTrainingOrderSentence::class.java),
+  DTO_ORA(sentenceClazz = DetentionAndTrainingOrderSentence::class.java),
+  BOTUS(sentenceClazz = BotusSentence::class.java),
+  //endregion
+
+  //region Standard Recall Sentences
+  LR(recallType = STANDARD_RECALL, isSDS40Eligible = true),
+  LR_ORA(recallType = STANDARD_RECALL, isSDS40Eligible = true),
+  LR_YOI_ORA(recallType = STANDARD_RECALL, isSDS40Eligible = true),
+  LR_SEC91_ORA(recallType = STANDARD_RECALL, isSDS40Eligible = true),
+  LRSEC250_ORA(recallType = STANDARD_RECALL, isSDS40Eligible = true),
   LR_EDS18(recallType = STANDARD_RECALL, sentenceClazz = ExtendedDeterminateSentence::class.java),
   LR_EDS21(recallType = STANDARD_RECALL, sentenceClazz = ExtendedDeterminateSentence::class.java),
   LR_EDSU18(recallType = STANDARD_RECALL, sentenceClazz = ExtendedDeterminateSentence::class.java),
@@ -72,23 +72,29 @@ enum class SentenceCalculationType(
   LR_SEC236A(recallType = STANDARD_RECALL, sentenceClazz = SopcSentence::class.java),
   LR_SOPC18(recallType = STANDARD_RECALL, sentenceClazz = SopcSentence::class.java),
   LR_SOPC21(recallType = STANDARD_RECALL, sentenceClazz = SopcSentence::class.java),
-  DTO(sentenceClazz = DetentionAndTrainingOrderSentence::class.java),
-  DTO_ORA(sentenceClazz = DetentionAndTrainingOrderSentence::class.java),
+  //endregion
+
+  //region Fixed Term Recall Sentences
+  FTR_14_ORA(recallType = FIXED_TERM_RECALL_14, primaryName = "14FTR_ORA", isSDS40Eligible = true),
+  FTR(recallType = FIXED_TERM_RECALL_28, isSDS40Eligible = true),
+  FTR_ORA(recallType = FIXED_TERM_RECALL_28, isSDS40Eligible = true),
+  FTR_SCH15(recallType = FIXED_TERM_RECALL_28, isSDS40Eligible = true),
+  FTRSCH15_ORA(recallType = FIXED_TERM_RECALL_28, isSDS40Eligible = true),
+  FTRSCH18(recallType = FIXED_TERM_RECALL_28, isSDS40Eligible = true),
+  FTRSCH18_ORA(recallType = FIXED_TERM_RECALL_28, isSDS40Eligible = true),
+  //endregion
+
+  //region Indeterminate Sentences
   IPP(sentenceClazz = IndeterminateSentence::class.java),
   LIFE(sentenceClazz = IndeterminateSentence::class.java),
   LIFE_IPP(sentenceClazz = IndeterminateSentence::class.java, primaryName = "LIFE/IPP"),
-  LR_IPP(sentenceClazz = IndeterminateSentence::class.java, recallType = STANDARD_RECALL),
   MLP(sentenceClazz = IndeterminateSentence::class.java),
   DLP(sentenceClazz = IndeterminateSentence::class.java),
   ALP(sentenceClazz = IndeterminateSentence::class.java),
   LEGACY(sentenceClazz = IndeterminateSentence::class.java),
-  LR_LIFE(sentenceClazz = IndeterminateSentence::class.java, recallType = STANDARD_RECALL),
   HMPL(sentenceClazz = IndeterminateSentence::class.java),
   DFL(sentenceClazz = IndeterminateSentence::class.java),
-  LR_ALP(sentenceClazz = IndeterminateSentence::class.java, recallType = STANDARD_RECALL),
   ALP_LASPO(sentenceClazz = IndeterminateSentence::class.java),
-  LR_DLP(sentenceClazz = IndeterminateSentence::class.java, recallType = STANDARD_RECALL),
-  LR_MLP(sentenceClazz = IndeterminateSentence::class.java, recallType = STANDARD_RECALL),
   SEC94(sentenceClazz = IndeterminateSentence::class.java),
   SEC93_03(sentenceClazz = IndeterminateSentence::class.java),
   ALP_CODE18(sentenceClazz = IndeterminateSentence::class.java),
@@ -96,32 +102,59 @@ enum class SentenceCalculationType(
   SEC272(sentenceClazz = IndeterminateSentence::class.java),
   SEC275(sentenceClazz = IndeterminateSentence::class.java),
   ALP_CODE21(sentenceClazz = IndeterminateSentence::class.java),
+  ZMD(sentenceClazz = IndeterminateSentence::class.java),
+  SEC93(sentenceClazz = IndeterminateSentence::class.java),
+  TWENTY(sentenceClazz = IndeterminateSentence::class.java, primaryName = "20"),
+  //endregion
+
+  //region Indeterminate Recall Sentences
+  LR_IPP(sentenceClazz = IndeterminateSentence::class.java, recallType = STANDARD_RECALL),
+  LR_LIFE(sentenceClazz = IndeterminateSentence::class.java, recallType = STANDARD_RECALL),
+  LR_ALP(sentenceClazz = IndeterminateSentence::class.java, recallType = STANDARD_RECALL),
+  LR_DLP(sentenceClazz = IndeterminateSentence::class.java, recallType = STANDARD_RECALL),
+  LR_MLP(sentenceClazz = IndeterminateSentence::class.java, recallType = STANDARD_RECALL),
   LR_DPP(sentenceClazz = IndeterminateSentence::class.java, recallType = STANDARD_RECALL),
   LR_ALP_CDE18(sentenceClazz = IndeterminateSentence::class.java, recallType = STANDARD_RECALL),
   LR_ALP_CDE21(sentenceClazz = IndeterminateSentence::class.java, recallType = STANDARD_RECALL),
   LR_ALP_LASPO(sentenceClazz = IndeterminateSentence::class.java, recallType = STANDARD_RECALL),
-  ZMD(sentenceClazz = IndeterminateSentence::class.java),
-  SEC93(sentenceClazz = IndeterminateSentence::class.java),
-  TWENTY(sentenceClazz = IndeterminateSentence::class.java, primaryName = "20"),
+  //endregion
+
+  //region Unsupported Sentence Types
   NP(sentenceClazz = UNSUPPORTED),
-  LR_EPP(sentenceClazz = UNSUPPORTED, recallType = STANDARD_RECALL),
   CR(sentenceClazz = UNSUPPORTED),
-  BOTUS(sentenceClazz = BotusSentence::class.java),
   AR(sentenceClazz = UNSUPPORTED),
   EPP(sentenceClazz = UNSUPPORTED),
-  CUR_ORA(sentenceClazz = UNSUPPORTED, recallType = STANDARD_RECALL),
   A_FINE(sentenceClazz = UNSUPPORTED, primaryName = "A/FINE"),
-  CUR(sentenceClazz = UNSUPPORTED, recallType = STANDARD_RECALL),
   CIVIL(sentenceClazz = UNSUPPORTED),
   EXT(sentenceClazz = UNSUPPORTED),
-  LR_ES(sentenceClazz = UNSUPPORTED, recallType = STANDARD_RECALL),
   YRO(sentenceClazz = UNSUPPORTED),
   SEC91(sentenceClazz = UNSUPPORTED),
   VOO(sentenceClazz = UNSUPPORTED),
   TISCS(sentenceClazz = UNSUPPORTED),
   STS21(sentenceClazz = UNSUPPORTED),
   STS18(sentenceClazz = UNSUPPORTED),
+  //endregion
+
+  //region Unsupported Recall Sentences
+  FTR_HDC(sentenceClazz = UNSUPPORTED, isSDS40Eligible = true, recallType = FIXED_TERM_RECALL_14),
+  LR_ES(sentenceClazz = UNSUPPORTED, recallType = STANDARD_RECALL),
+  LR_EPP(sentenceClazz = UNSUPPORTED, recallType = STANDARD_RECALL),
+  FTR_HDC_ORA(recallType = FIXED_TERM_RECALL_28, sentenceClazz = UNSUPPORTED, isSDS40Eligible = true),
+  FTR_14_HDC_ORA(
+    recallType = FIXED_TERM_RECALL_14,
+    primaryName = "14FTRHDC_ORA",
+    sentenceClazz = UNSUPPORTED,
+    isSDS40Eligible = true,
+  ),
+  HDR_ORA(sentenceClazz = UNSUPPORTED, isSDS40Eligible = true, recallType = STANDARD_RECALL_255),
+  HDR(sentenceClazz = UNSUPPORTED, isSDS40Eligible = true, recallType = STANDARD_RECALL_255),
+  CUR(sentenceClazz = UNSUPPORTED, recallType = STANDARD_RECALL_255),
+  CUR_ORA(sentenceClazz = UNSUPPORTED, recallType = STANDARD_RECALL_255),
+  //endregion
+
+  //region Unidentified
   UNIDENTIFIED(sentenceClazz = UNSUPPORTED),
+  //endregion
   ;
 
   companion object {
