9d7d36c78ff2496e44c6da579396bdf0466f3331
Luke Harper <luke.harper@digital.justice.gov.uk>
2025-04-08 11:18:52 +0100

    CRS-2237 Split external movements feature toggle into two.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/config/FeatureToggles.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/config/FeatureToggles.kt
@@ -5,7 +5,15 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 @ConfigurationProperties(prefix = "feature-toggles")
 data class FeatureToggles(
   var supportInactiveSentencesAndAdjustments: Boolean = false,
-  var externalMovementsEnabled: Boolean = false,
   var useAdjustmentsApi: Boolean = false,
   var concurrentConsecutiveSentencesEnabled: Boolean = false,
-)
+  var externalMovementsSds40: Boolean = false,
+  var externalMovementsAdjustmentSharing: Boolean = false,
+) {
+  
+  val externalMovementsEnabled: Boolean
+    get() {
+      return externalMovementsSds40 || externalMovementsAdjustmentSharing
+    }
+}
+
