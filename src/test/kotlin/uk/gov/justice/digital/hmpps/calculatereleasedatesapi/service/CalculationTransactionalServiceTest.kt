f388b99e70f8302db56ede7d3bd180e87d6c8b13
matthew.green <matthew.green3@digital.justice.gov.uk>
2025-06-18 14:41:41 +0100

    CRS-2364-immediate-release-recall-ual-bug
    Add ticket AC coverage

--- src/test/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/CalculationTransactionalServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/CalculationTransactionalServiceTest.kt
@@ -184,11 +184,6 @@ class CalculationTransactionalServiceTest {
   @Captor
   lateinit var updatedOffenderDatesArgumentCaptor: ArgumentCaptor<UpdateOffenderDates>
 
-  @Test
-  fun blah() {
-    `Test Example`("custom-examples", "blah")
-  }
-
   @ParameterizedTest
   @CsvFileSource(resources = ["/test_data/calculation-service-examples.csv"], numLinesToSkip = 1)
   fun `Test Example`(
