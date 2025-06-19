31a2f6f9133fcd4813d198e4c3025963417d5a7b
Joel Stobart (MOJ) <87521440+joelstobart-moj@users.noreply.github.com>
2025-06-19 17:22:07 +0100

    Vary parameters for ERS using a new period parameter in the configuration (#1120)
    * Vary parameters for ERS using a new period parameter in the configuration
    
    * Change the preprod param to 4 years
    
    ---------
    
    Co-authored-by: Joel Stobart <>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/config/ErsedConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/config/ErsedConfiguration.kt
@@ -1,9 +1,10 @@
 package uk.gov.justice.digital.hmpps.calculatereleasedatesapi.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import java.time.Period
 
 @ConfigurationProperties("ersed")
 data class ErsedConfiguration(
-  val maxPeriodDays: Int,
+  val maxPeriod: Period,
   val releasePoint: Double,
 )
