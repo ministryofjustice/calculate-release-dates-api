84f4ec8bc03719805f68dc1214e692e9c9d9dd2b
Luke Harper <luke.harper@digital.justice.gov.uk>
2025-07-22 17:18:58 +0100

    remove failing migration.

--- src/main/resources/migration/common/V71__fix_status_column.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM comparison_status where id in (0,1,2,3);
-
-insert into comparison_status (id, name) values (0, 'SETUP');
-insert into comparison_status (id, name) values (1, 'PROCESSING');
-insert into comparison_status (id, name) values (2, 'COMPLETED');
-insert into comparison_status (id, name) values (3, 'ERROR');
