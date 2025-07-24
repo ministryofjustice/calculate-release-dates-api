38f138789c92308a5ec557c65b4acaec28d28a13
james-baker-moj <159048573+james-baker-moj@users.noreply.github.com>
2025-07-24 15:00:07 +0100

    Initial support for ERS30 (#1159)
    * CRS-2381: Initial support for ERS30
    
    When the feature toggle is enabled calculate both ERS30 and ERS50 dates. If ERS50 excluding ADAs + RADAs is before commencement, then retain ERS50. If ERS30 excluding ADAs + RADAs is before commencement, default to the commencement date. Otherwise, use the ERS30.
    
    * CRS-2381: Remove test for acceptance criteria L
    
    Pending further investigation into whether ERSED should be calculated in this case or not as it is generated both before and after these changes.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/config/ErsedConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/config/ErsedConfiguration.kt
@@ -1,10 +1,14 @@
 package uk.gov.justice.digital.hmpps.calculatereleasedatesapi.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import java.time.Period
+import java.time.temporal.ChronoUnit
 
 @ConfigurationProperties("ersed")
 data class ErsedConfiguration(
-  val maxPeriod: Period,
-  val releasePoint: Double,
+  val ers50MaxPeriodUnit: ChronoUnit,
+  val ers50MaxPeriodAmount: Long,
+  val ers50ReleasePoint: Double,
+  val ers30MaxPeriodUnit: ChronoUnit,
+  val ers30MaxPeriodAmount: Long,
+  val ers30ReleasePoint: Double,
 )
--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/ErsedCalculator.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/ErsedCalculator.kt
@@ -12,7 +12,10 @@ import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.CalculableSen
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.ReleaseDateCalculationBreakdown
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.SentenceCalculation
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.StandardDeterminateSentence
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.util.isAfterOrEqualTo
+import java.time.LocalDate
 import java.time.temporal.ChronoUnit
+import kotlin.collections.set
 import kotlin.math.ceil
 
 @Service
@@ -21,94 +24,137 @@ class ErsedCalculator(
   private val featureToggles: FeatureToggles,
 ) {
 
-  fun generateEarlyReleaseSchemeEligibilityDateBreakdown(
-    sentence: CalculableSentence,
-    sentenceCalculation: SentenceCalculation,
-  ) {
-    if (featureToggles.useERS30Calculation) {
-      log.info("Using ERS30 ERSED algorithm")
-      calculateERSEDUsingERS30Algorithm(sentence, sentenceCalculation)
-    } else {
-      log.info("Using legacy ERSED algorithm")
-      calculateERSEDUsingLegacyAlgorithm(sentence, sentenceCalculation)
+  fun generateEarlyReleaseSchemeEligibilityDateBreakdown(sentence: CalculableSentence, sentenceCalculation: SentenceCalculation) {
+    if (isSentenceEligible(sentence)) {
+      if (featureToggles.useERS30Calculation) {
+        log.info("Including ERS30 and ERS50 in ERSED calculation")
+        calculateUsingBothERS30AndERS50(sentence, sentenceCalculation)
+      } else {
+        log.info("Using only ERS50 in ERSED calculation")
+        calculateUsingERS50Only(sentence, sentenceCalculation)
+      }
     }
   }
 
-  private fun calculateERSEDUsingERS30Algorithm(sentence: CalculableSentence, sentenceCalculation: SentenceCalculation) {
-    // TODO ERS30 implementation
-    calculateERSEDUsingLegacyAlgorithm(sentence, sentenceCalculation)
+  private fun calculateUsingBothERS30AndERS50(sentence: CalculableSentence, sentenceCalculation: SentenceCalculation) {
+    val ers50Result = calculate(
+      Params(
+        sentence = sentence,
+        sentenceCalculation = sentenceCalculation,
+        maxPeriodUnit = ersedConfiguration.ers50MaxPeriodUnit,
+        maxPeriodAmount = ersedConfiguration.ers50MaxPeriodAmount,
+        releasePoint = ersedConfiguration.ers50ReleasePoint,
+      ),
+    )
+    val ers30Result = calculate(
+      Params(
+        sentence = sentence,
+        sentenceCalculation = sentenceCalculation,
+        maxPeriodUnit = ersedConfiguration.ers30MaxPeriodUnit,
+        maxPeriodAmount = ersedConfiguration.ers30MaxPeriodAmount,
+        releasePoint = ersedConfiguration.ers30ReleasePoint,
+      ),
+    )
+
+    if (ers50Result.adjustedDateExcludingAwarded.isBefore(ImportantDates.ERS30_COMMENCEMENT_DATE)) {
+      sentenceCalculation.breakdownByReleaseDateType[ReleaseDateType.ERSED] = ers50Result.breakdown
+    } else if (ers30Result.adjustedDateExcludingAwarded.isAfterOrEqualTo(ImportantDates.ERS30_COMMENCEMENT_DATE)) {
+      sentenceCalculation.breakdownByReleaseDateType[ReleaseDateType.ERSED] = ers30Result.breakdown
+    } else {
+      val daysToBeAddedExcludingUAL = sentenceCalculation.adjustments.awardedDuringCustody -
+        sentenceCalculation.adjustments.unusedAdaDays -
+        sentenceCalculation.adjustments.servedAdaDays
+      sentenceCalculation.breakdownByReleaseDateType[ReleaseDateType.ERSED] = ReleaseDateCalculationBreakdown(
+        releaseDate = ImportantDates.ERS30_COMMENCEMENT_DATE.plusDays(daysToBeAddedExcludingUAL),
+        unadjustedDate = ImportantDates.ERS30_COMMENCEMENT_DATE,
+        adjustedDays = daysToBeAddedExcludingUAL,
+        rules = setOf(CalculationRule.ERSED_ADJUSTED_TO_ERS30_COMMENCEMENT),
+      )
+    }
   }
 
-  private fun calculateERSEDUsingLegacyAlgorithm(sentence: CalculableSentence, sentenceCalculation: SentenceCalculation) {
-    val ersed = calculateErsed(sentence, sentenceCalculation) ?: return
-
-    val addedDays = sentenceCalculation.adjustments.ualDuringCustody + sentenceCalculation.adjustments.awardedDuringCustody
-    val adjustedErsed = ersed.releaseDate.minusDays(addedDays)
-
-    sentenceCalculation.breakdownByReleaseDateType[ReleaseDateType.ERSED] =
-      if (adjustedErsed.isBefore(sentence.sentencedAt)) {
-        ReleaseDateCalculationBreakdown(
-          releaseDate = sentence.sentencedAt.plusDays(addedDays),
-          unadjustedDate = sentence.sentencedAt,
-          adjustedDays = addedDays,
-          rules = setOf(CalculationRule.ERSED_BEFORE_SENTENCE_DATE),
-        )
-      } else {
-        ersed
-      }
+  private fun calculateUsingERS50Only(sentence: CalculableSentence, sentenceCalculation: SentenceCalculation) {
+    val params = Params(
+      sentence = sentence,
+      sentenceCalculation = sentenceCalculation,
+      maxPeriodUnit = ersedConfiguration.ers50MaxPeriodUnit,
+      maxPeriodAmount = ersedConfiguration.ers50MaxPeriodAmount,
+      releasePoint = ersedConfiguration.ers50ReleasePoint,
+    )
+    sentenceCalculation.breakdownByReleaseDateType[ReleaseDateType.ERSED] = calculate(params).breakdown
   }
 
-  private fun calculateErsed(
-    sentence: CalculableSentence,
-    sentenceCalculation: SentenceCalculation,
-  ): ReleaseDateCalculationBreakdown? = when {
-    sentence.isRecall() -> null
-    !sentence.calculateErsed() -> null
-    !isEligibleUnderCJAAndLASPO(sentence) -> null
-    else -> calculateErsedMinOrMax(sentence, sentenceCalculation)
+  private fun isSentenceEligible(sentence: CalculableSentence): Boolean = when {
+    sentence.isRecall() -> false
+    !sentence.calculateErsed() -> false
+    !isEligibleUnderCJAAndLASPO(sentence) -> false
+    else -> true
   }
 
   private fun isEligibleUnderCJAAndLASPO(sentence: CalculableSentence): Boolean = sentence !is StandardDeterminateSentence || !sentence.isBeforeCJAAndLASPO()
 
-  private fun calculateErsedMinOrMax(
-    sentence: CalculableSentence,
-    sentenceCalculation: SentenceCalculation,
-  ): ReleaseDateCalculationBreakdown {
-    val isExtended = sentenceCalculation.extendedDeterminateParoleEligibilityDate != null
-    val effectiveRelease = if (isExtended) {
-      sentenceCalculation.extendedDeterminateParoleEligibilityDate!!
+  private data class Params(
+    val sentence: CalculableSentence,
+    val sentenceCalculation: SentenceCalculation,
+    val maxPeriodUnit: ChronoUnit,
+    val maxPeriodAmount: Long,
+    val releasePoint: Double,
+  )
+
+  private data class Result(val breakdown: ReleaseDateCalculationBreakdown, val adjustedDateExcludingAwarded: LocalDate)
+
+  private fun calculate(params: Params): Result {
+    val ersed = calculateErsedMinOrMax(params)
+
+    val addedDays = params.sentenceCalculation.adjustments.ualDuringCustody + params.sentenceCalculation.adjustments.awardedDuringCustody
+    val adjustedErsed = ersed.releaseDate.minusDays(addedDays)
+
+    return if (adjustedErsed.isBefore(params.sentence.sentencedAt)) {
+      Result(
+        ReleaseDateCalculationBreakdown(
+          releaseDate = params.sentence.sentencedAt.plusDays(addedDays),
+          unadjustedDate = params.sentence.sentencedAt,
+          adjustedDays = addedDays,
+          rules = setOf(CalculationRule.ERSED_BEFORE_SENTENCE_DATE),
+        ),
+        adjustedErsed.plusDays(params.sentenceCalculation.adjustments.ualDuringCustody),
+      )
     } else {
-      sentenceCalculation.adjustedDeterminateReleaseDate
+      Result(ersed, adjustedErsed.plusDays(params.sentenceCalculation.adjustments.ualDuringCustody))
+    }
+  }
+
+  private fun calculateErsedMinOrMax(params: Params): ReleaseDateCalculationBreakdown {
+    val isExtended = params.sentenceCalculation.extendedDeterminateParoleEligibilityDate != null
+    val effectiveRelease = if (isExtended) {
+      params.sentenceCalculation.extendedDeterminateParoleEligibilityDate!!
+    } else {
+      params.sentenceCalculation.adjustedDeterminateReleaseDate
     }
 
-    val unadjustedEffectiveRelease = sentenceCalculation.unadjustedExtendedDeterminateParoleEligibilityDate
-      ?: sentenceCalculation.unadjustedReleaseDate.unadjustedDeterminateReleaseDate
+    val unadjustedEffectiveRelease = params.sentenceCalculation.unadjustedExtendedDeterminateParoleEligibilityDate
+      ?: params.sentenceCalculation.unadjustedReleaseDate.unadjustedDeterminateReleaseDate
 
-    val maxEffectiveErsed = effectiveRelease.minus(ersedConfiguration.maxPeriod)
-
-    val (unit, amount) = when {
-      ersedConfiguration.maxPeriod.years > 0 -> ChronoUnit.YEARS to -ersedConfiguration.maxPeriod.years.toLong()
-      else -> ChronoUnit.DAYS to -ersedConfiguration.maxPeriod.days.toLong()
-    }
+    val maxEffectiveErsed = effectiveRelease.minus(params.maxPeriodAmount, params.maxPeriodUnit)
 
     val maxEffectiveErsedBreakdown = ReleaseDateCalculationBreakdown(
       rules = setOf(CalculationRule.ERSED_MAX_PERIOD),
       releaseDate = maxEffectiveErsed,
       unadjustedDate = unadjustedEffectiveRelease,
       adjustedDays = ChronoUnit.DAYS.between(
-        sentenceCalculation.unadjustedReleaseDate.unadjustedDeterminateReleaseDate,
-        sentenceCalculation.adjustedDeterminateReleaseDate,
+        params.sentenceCalculation.unadjustedReleaseDate.unadjustedDeterminateReleaseDate,
+        params.sentenceCalculation.adjustedDeterminateReleaseDate,
       ),
       rulesWithExtraAdjustments = mapOf(
-        CalculationRule.ERSED_MAX_PERIOD to AdjustmentDuration(amount, unit),
+        CalculationRule.ERSED_MAX_PERIOD to AdjustmentDuration(-params.maxPeriodAmount, params.maxPeriodUnit),
       ),
     )
 
-    val daysUntilRelease = ChronoUnit.DAYS.between(sentence.sentencedAt, unadjustedEffectiveRelease).plus(1).toInt()
-    val unadjustedMinimumErsed = sentence.sentencedAt
-      .plusDays(ceil(daysUntilRelease * ersedConfiguration.releasePoint).toLong())
+    val daysUntilRelease = ChronoUnit.DAYS.between(params.sentence.sentencedAt, unadjustedEffectiveRelease).plus(1).toInt()
+    val unadjustedMinimumErsed = params.sentence.sentencedAt
+      .plusDays(ceil(daysUntilRelease * params.releasePoint).toLong())
     val minimumEffectiveErsed = unadjustedMinimumErsed
-      .plusDays(sentenceCalculation.adjustments.adjustmentsForInitialRelease())
+      .plusDays(params.sentenceCalculation.adjustments.adjustmentsForInitialRelease())
 
     val minimumEffectiveErsedBreakdown = ReleaseDateCalculationBreakdown(
       rules = setOf(CalculationRule.ERSED_MIN_EFFECTIVE_DATE),
