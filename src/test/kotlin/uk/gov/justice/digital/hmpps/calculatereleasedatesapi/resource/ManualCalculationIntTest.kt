4a1914746550cb3178afefef1d689f98d40594b0
matthew.green <matthew.green3@digital.justice.gov.uk>
2025-09-06 10:56:55 +0100

    crs-2437-check-for-last-last-manual-calc
    Add integration tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/resource/ManualCalculationIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/resource/ManualCalculationIntTest.kt
@@ -97,6 +97,62 @@ class ManualCalculationIntTest : IntegrationTestBase() {
     assertThat(response.calculationRequestId).isNotNull
   }
 
+  @Test
+  fun `Confirm previous manual calculation does not exists for SHPO validation fail`() {
+    val response = webTestClient.get()
+      .uri("/manual-calculation/CRS-2437/has-existing-calculation")
+      .accept(MediaType.APPLICATION_JSON)
+      .headers(setAuthorisation(roles = listOf("ROLE_RELEASE_DATES_CALCULATOR")))
+      .exchange()
+      .expectStatus().isOk
+      .expectHeader().contentType(MediaType.APPLICATION_JSON)
+      .expectBody(Boolean::class.java)
+      .returnResult().responseBody!!
+    assertThat(response).isEqualTo(false)
+  }
+
+  @Test
+  fun `Confirm previous manual calculation exists for SHPO validation fail`() {
+    val newCalculation = webTestClient.post()
+      .uri("/manual-calculation/CRS-2437")
+      .bodyValue(
+        ManualEntryRequest(
+          reasonForCalculationId = 1,
+          otherReasonDescription = "",
+          selectedManualEntryDates = listOf(
+            ManualEntrySelectedDate(
+              ReleaseDateType.PED,
+              "PED",
+              SubmittedDate(
+                day = 1,
+                month = 1,
+                year = 2040,
+              ),
+            ),
+          ),
+        ),
+      )
+      .accept(MediaType.APPLICATION_JSON)
+      .headers(setAuthorisation(roles = listOf("ROLE_RELEASE_DATES_CALCULATOR")))
+      .exchange()
+      .expectStatus().isOk
+      .expectHeader().contentType(MediaType.APPLICATION_JSON)
+      .expectBody(ManualCalculationResponse::class.java)
+      .returnResult().responseBody!!
+    assertThat(newCalculation.calculationRequestId).isNotNull
+
+    val previousCalculationCheck = webTestClient.get()
+      .uri("/manual-calculation/CRS-2437/has-existing-calculation")
+      .accept(MediaType.APPLICATION_JSON)
+      .headers(setAuthorisation(roles = listOf("ROLE_RELEASE_DATES_CALCULATOR")))
+      .exchange()
+      .expectStatus().isOk
+      .expectHeader().contentType(MediaType.APPLICATION_JSON)
+      .expectBody(Boolean::class.java)
+      .returnResult().responseBody!!
+    assertThat(previousCalculationCheck).isEqualTo(true)
+  }
+
   companion object {
     private const val PRISONER_ID = "default"
     val BOOKING_ID = PRISONER_ID.hashCode().toLong()
