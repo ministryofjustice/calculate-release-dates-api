c47fa674519e680f043ba8affcdc957f5f3adcd0
Matthew Green <matthew.green3@justice.gov.uk>
2025-09-24 13:20:52 +0100

    crs-2480-pss-repeal
    remove test method

--- src/test/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/calculation/StandardConfigurationTests.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/calculation/StandardConfigurationTests.kt
@@ -7,7 +7,6 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.io.File
 import java.util.stream.Stream
-import kotlin.test.Test
 
 class StandardConfigurationTests : CalculationExampleTests() {
 
