793945504b4403c8c84f47d8b55acbaac2be29b8
james-baker-moj <159048573+james-baker-moj@users.noreply.github.com>
2025-09-24 15:48:19 +0100

    CRS-2406: Update genuine override reasons (#1212)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/GenuineOverrideReason.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/GenuineOverrideReason.kt
@@ -1,13 +1,14 @@
 package uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model
 
 enum class GenuineOverrideReason(val description: String, val displayOrder: Int, val requiresFurtherDetail: Boolean) {
-  RECORD_CROSS_BORDER_SECTION_RELEASE_DATES("To record cross-border section release dates", 0, false),
-  RECORD_POWER_TO_DETAIN("To record ‘Power to detain’", 1, false),
-  RECORD_ERS_BREACH("To record an ERS (Early release scheme) breach", 2, false),
-  ADD_RELEASE_DATES_FROM_PREVIOUS_BOOKING("To add release dates from a previous booking", 3, false),
-  MISALIGNED_COURT_DOCUMENTS("There are misaligned court documents", 4, false),
-  UNSUPPORTED_SENTENCES("The calculation includes unsupported sentences", 5, false),
-  OTHER("Other", 6, true),
+  ORDER_OF_IMPRISONMENT_OR_WARRANT_DOES_NOT_MATCH_TRIAL_RECORD("Order of imprisonment/warrant doesn't match trial record sheet", 0, false),
+  TERRORISM("Terrorism or terror-related offences", 1, false),
+  POWER_TO_DETAIN("Power to detain", 2, false),
+  CROSS_BORDER_SECTION_RELEASE_DATE("Cross border section release dates", 3, false),
+  ADD_RELEASE_DATE_FROM_ANOTHER_BOOKING("Adding a release date from another booking", 4, false),
+  ERS_BREACH("ERS (Early release scheme) breach", 5, false),
+  COURT_OF_APPEAL("Court of appeal", 6, false),
+  OTHER("The reason is not on this list", 7, true),
   ;
 
   fun toResponse() = GenuineOverrideReasonResponse(
