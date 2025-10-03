f081b16ba8abc9f377089b5a9d7655b332d9332e
james-baker-moj <159048573+james-baker-moj@users.noreply.github.com>
2025-10-03 14:20:22 +0100

    CRS-2426: Add genuine override details to calc history (#1227)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/HistoricCalculationsServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/HistoricCalculationsServiceTest.kt
@@ -15,6 +15,7 @@ import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.enumerations.Calcul
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.Agency
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.CalculationSource
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.CalculationViewConfiguration
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.GenuineOverrideReason
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.SentenceCalculationSummary
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.repository.CalculationRequestRepository
 import java.time.LocalDateTime
@@ -73,6 +74,37 @@ class HistoricCalculationsServiceTest {
     assertThat(result[0].calculationSource).isEqualTo(CalculationSource.NOMIS)
     assertThat(result[0].calculationViewConfiguration).isNull()
     assertThat(result[0].establishment).isNull()
+  }
+
+  @Test
+  fun `Genuine override details added if CRDS calculation found in database`() {
+    val calcRequest1 = calculationRequest().copy(genuineOverrideReason = GenuineOverrideReason.TERRORISM, genuineOverrideReasonFurtherDetail = null)
+    val calcRequest2 = calcRequest1.copy(
+      prisonerLocation = "KTI",
+      calculationReference = UUID.randomUUID(),
+      reasonForCalculation = CalculationReason(id = 1, isActive = true, isOther = false, displayName = "calc reason", isBulk = false, nomisReason = null, nomisComment = null, displayRank = 1),
+      genuineOverrideReason = GenuineOverrideReason.OTHER,
+      genuineOverrideReasonFurtherDetail = "Some more details",
+    )
+    whenever(calculationRequestRepository.findAllByPrisonerIdAndCalculationStatus(anyString(), anyString())).thenReturn(listOf(calcRequest1, calcRequest2))
+
+    val sentenceCalculationSummary1 = sentenceCalculationSummary("comment $reference")
+    val sentenceCalculationSummary2 = sentenceCalculationSummary1.copy(bookingId = 3, commentText = "comment: ${calcRequest2.calculationReference}")
+    whenever(prisonService.getCalculationsForAPrisonerId(anyString())).thenReturn(listOf(sentenceCalculationSummary1, sentenceCalculationSummary2))
+    val result = underTest.getHistoricCalculationsForPrisoner("123")
+    assertThat(result).hasSize(2)
+    assertThat(result[0].calculationSource).isEqualTo(CalculationSource.CRDS)
+    assertThat(result[0].calculationViewConfiguration).isEqualTo(CalculationViewConfiguration(reference.toString(), 1))
+    assertThat(result[0].establishment).isEqualTo("Chelmsford (HMP)")
+    assertThat(result[0].genuineOverrideReasonCode).isEqualTo(GenuineOverrideReason.TERRORISM)
+    assertThat(result[0].genuineOverrideReasonDescription).isEqualTo("Terrorism or terror-related offences")
+
+    assertThat(result[1].calculationSource).isEqualTo(CalculationSource.CRDS)
+    assertThat(result[1].calculationViewConfiguration).isEqualTo(CalculationViewConfiguration(calcRequest2.calculationReference.toString(), 1))
+    assertThat(result[1].establishment).isEqualTo("HMP KENNET")
+    assertThat(result[1].calculationReason).isEqualTo(calcRequest2.reasonForCalculation?.displayName)
+    assertThat(result[1].genuineOverrideReasonCode).isEqualTo(GenuineOverrideReason.OTHER)
+    assertThat(result[1].genuineOverrideReasonDescription).isEqualTo("Some more details")
   }
 
   private fun sentenceCalculationSummary(comment: String): SentenceCalculationSummary = SentenceCalculationSummary(456, "123", "bob", "davies", "RNI", "Ranby (HMP)", 1, LocalDateTime.now(), 4, comment, "reason", "user")
