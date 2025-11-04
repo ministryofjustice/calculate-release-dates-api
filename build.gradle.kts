fe9beb712c9560a75c74dde46208af46e8eb684a
Matthew Green <matthew.green3@justice.gov.uk>
2025-11-04 10:26:20 +0000

    crs-2525-things-todo-sentence-check
    Include all open plugin for kotlin data classes and Hibernate.
    
    Exclude application-local.yml from GIT

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
   id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
   kotlin("plugin.spring") version "2.2.20"
   kotlin("plugin.jpa") version "2.2.20"
+  kotlin("plugin.allopen") version "2.2.21"
   id("jacoco")
   id("org.openapi.generator") version "7.16.0"
 }
