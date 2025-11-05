b3779e64228b3b0f58f49accbbd08373f85f4ba2
ldlharper <luke.harper@digital.justice.gov.uk>
2025-11-05 13:47:02 +0000

    RCLL-551 Remove old recalls endpoint. (#1261)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/resource/RecordARecallController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/resource/RecordARecallController.kt
@@ -16,40 +16,14 @@ import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.RecordARecallDecisionResult
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.RecordARecallRequest
-import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.RecordARecallResult
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.RecordARecallValidationResult
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.service.RecordARecallDecisionService
-import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.service.RecordARecallService
 
 @RestController
 @Tag(name = "record-a-recall-controller", description = "Operations involving a calculation")
 class RecordARecallController(
-  private val recordARecallService: RecordARecallService,
   private val recordARecallDecisionService: RecordARecallDecisionService,
 ) {
-  @PostMapping(value = ["/record-a-recall/{prisonerId}"])
-  @PreAuthorize("hasAnyRole('RECORD_A_RECALL', 'CALCULATE_RELEASE_DATES__RECALL__CALCULATE__RW')")
-  @ResponseBody
-  @Operation(
-    summary = "Calculate release dates for a prisoner - used explicitly by the record-a-recall service, this does not publish to NOMIS",
-    description = "This endpoint will calculate release dates based on a prisoners latest booking - this is a transitory calculation that will not be published to NOMIS",
-  )
-  @ApiResponses(
-    value = [
-      ApiResponse(responseCode = "200", description = "Returns calculated dates"),
-      ApiResponse(responseCode = "401", description = "Unauthorised, requires a valid Oauth2 token"),
-      ApiResponse(responseCode = "403", description = "Forbidden, requires an appropriate role"),
-      ApiResponse(responseCode = "422", description = "Unprocessable request, the existing data cannot be used to perform a calculation"),
-    ],
-  )
-  fun calculateForRecall(
-    @Parameter(required = true, example = "A1234AB", description = "The prisoners ID (aka nomsId)")
-    @PathVariable("prisonerId")
-    prisonerId: String,
-  ): RecordARecallResult {
-    log.info("Request received to calculate release dates for a recall of $prisonerId")
-    return recordARecallService.calculateAndValidateForRecordARecall(prisonerId)
-  }
 
   @PostMapping(value = ["/record-a-recall/{prisonerId}/decision"])
   @PreAuthorize("hasAnyRole('RECORD_A_RECALL', 'CALCULATE_RELEASE_DATES__RECALL__CALCULATE__RW')")
