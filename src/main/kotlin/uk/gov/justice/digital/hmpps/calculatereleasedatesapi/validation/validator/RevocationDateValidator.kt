41a62808c951263e50edef9eb50a4fdfe8bdef71
Matthew Green <matthew.green3@justice.gov.uk>
2025-11-19 10:03:03 +0000

    crs-2465-remove-ftr56-revocation-date-validation
    FTR56 no longer relies on a revocation date.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/validation/validator/RevocationDateValidator.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/validation/validator/RevocationDateValidator.kt
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.hmpps.calculatereleasedatesapi.validation.validator
 
 import org.springframework.stereotype.Component
-import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.RecallType.FIXED_TERM_RECALL_56
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.external.CalculationSourceData
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.external.SentenceCalculationType.Companion.from
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.validation.ValidationCode
@@ -13,12 +12,10 @@ import java.time.LocalDate
 class RevocationDateValidator : PreCalculationSourceDataValidator {
 
   override fun validate(sourceData: CalculationSourceData): List<ValidationMessage> {
-    val recallSentences = sourceData.sentenceAndOffences.filter { from(it.sentenceCalculationType).recallType != null }
-    val revocationDate = sourceData.findLatestRevocationDate()
-    if (recallSentences.isNotEmpty()) {
-      if (revocationDate == null && recallSentences.any { from(it.sentenceCalculationType).recallType == FIXED_TERM_RECALL_56 }) {
-        return listOf(ValidationMessage(ValidationCode.RECALL_MISSING_REVOCATION_DATE))
-      }
+    val recallSentences = sourceData.sentenceAndOffences.any { from(it.sentenceCalculationType).recallType != null }
+
+    if (recallSentences) {
+      val revocationDate = sourceData.findLatestRevocationDate()
       if (revocationDate != null && revocationDate.isAfter(LocalDate.now())) {
         return listOf(ValidationMessage(ValidationCode.REVOCATION_DATE_IN_THE_FUTURE))
       }
