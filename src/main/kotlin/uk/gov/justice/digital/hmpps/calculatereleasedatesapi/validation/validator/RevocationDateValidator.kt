c813ddebeeba5a5d7f6f11568a9d9f6547370dfb
Matthew Green <matthew.green3@justice.gov.uk>
2025-12-03 10:14:07 +0000

    crs-2465-reinstate-revocation-date-validation
    In addition, exit FixedTermRecallBookingValidator if no FTR14 or FTR28 sentences are present, now FTR56 exists.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/validation/validator/RevocationDateValidator.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/validation/validator/RevocationDateValidator.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.calculatereleasedatesapi.validation.validator
 
 import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.RecallType.FIXED_TERM_RECALL_56
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.external.CalculationSourceData
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.external.SentenceCalculationType.Companion.from
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.validation.ValidationCode
@@ -12,10 +13,14 @@ import java.time.LocalDate
 class RevocationDateValidator : PreCalculationSourceDataValidator {
 
   override fun validate(sourceData: CalculationSourceData): List<ValidationMessage> {
-    val recallSentences = sourceData.sentenceAndOffences.any { from(it.sentenceCalculationType).recallType != null }
+    val recallSentences = sourceData.sentenceAndOffences.filter { from(it.sentenceCalculationType).recallType != null }
 
-    if (recallSentences) {
-      val revocationDate = sourceData.findLatestRevocationDate()
+    val revocationDate = sourceData.findLatestRevocationDate()
+
+    if (recallSentences.isNotEmpty()) {
+      if (revocationDate == null && recallSentences.any { from(it.sentenceCalculationType).recallType == FIXED_TERM_RECALL_56 }) {
+        return listOf(ValidationMessage(ValidationCode.RECALL_MISSING_REVOCATION_DATE))
+      }
       if (revocationDate != null && revocationDate.isAfter(LocalDate.now())) {
         return listOf(ValidationMessage(ValidationCode.REVOCATION_DATE_IN_THE_FUTURE))
       }
