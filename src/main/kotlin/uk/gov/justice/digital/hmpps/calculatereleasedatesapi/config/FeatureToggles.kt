c51850fc2f99ee2d9d95bc38759d17f4dea281b9
Luke Harper <luke.harper@digital.justice.gov.uk>
2025-12-05 12:23:29 +0000

    RCLL-562 FTR-56 recall types functionality, RCLL-565 Extra tests.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/config/FeatureToggles.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/config/FeatureToggles.kt
@@ -13,6 +13,7 @@ data class FeatureToggles(
   var ftr48ManualJourney: Boolean = false,
   var useERS30Calculation: Boolean = false,
   var applyPostRecallRepealRules: Boolean = false,
+  var recordARecallFtr56Rules: Boolean = false,
 ) {
 
   val externalMovementsEnabled: Boolean
