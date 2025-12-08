515b896628983e8af07f7d5b260b0e3a92d5aead
James Baker <james.baker3@digital.justice.gov.uk>
2025-12-08 10:47:32 +0000

    CRS-2553: Check for unused deductions on previous SLED calc
    If the 2 day check calculation that we might use for a previously recorded SLED contains unused deductions then we do not use the previously recorded SLED.
    When someone comes back in on a recall and they have unused remand it means that their SLED will automatically be earlier than the previous SLED.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/PreviouslyRecordedSLEDService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/PreviouslyRecordedSLEDService.kt
@@ -1,18 +1,25 @@
 package uk.gov.justice.digital.hmpps.calculatereleasedatesapi.service
 
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.adjustmentsapi.model.AdjustmentDto
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.entity.CalculationOutcome
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.enumerations.ReleaseDateType
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.PreviouslyRecordedSLED
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.repository.CalculationOutcomeRepository
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.repository.CalculationRequestRepository
 import java.time.LocalDate
+import kotlin.jvm.optionals.getOrElse
 
 @Service
-class DominantHistoricDateService(private val calculationOutcomeRepository: CalculationOutcomeRepository) {
+class PreviouslyRecordedSLEDService(
+  private val calculationOutcomeRepository: CalculationOutcomeRepository,
+  private val calculationRequestRepository: CalculationRequestRepository,
+  private val sourceDataMapper: SourceDataMapper,
+) {
 
   fun findPreviouslyRecordedSLEDThatShouldOverrideTheCalculatedSLED(prisonerId: String, calculatedSLED: LocalDate): PreviouslyRecordedSLED? {
-    val dominantDates = calculationOutcomeRepository.getPotentialDatesForPreviouslyRecordedSLED(prisonerId, calculatedSLED)
-    return dominantDates
+    val potentialDates = calculationOutcomeRepository.getPotentialDatesForPreviouslyRecordedSLED(prisonerId, calculatedSLED)
+    val bestPossibleDate = potentialDates
       .groupBy { it.calculationRequestId }
       .filter { (_, dates) -> dates.any { it.calculationDateType == ReleaseDateType.SLED.name } || hasSedAndLedWithTheSameDate(dates) }
       .mapNotNull { (calculationRequestId, dates) ->
@@ -25,6 +32,18 @@ class DominantHistoricDateService(private val calculationOutcomeRepository: Calc
           )
         }
       }.maxByOrNull { it.previouslyRecordedSLEDDate }
+    return if (bestPossibleDate?.let { didNotHaveUnusedDeduction(it) } ?: false) {
+      bestPossibleDate
+    } else {
+      null
+    }
+  }
+
+  private fun didNotHaveUnusedDeduction(previousSLED: PreviouslyRecordedSLED): Boolean {
+    val calcRequest = calculationRequestRepository.findById(previousSLED.previouslyRecordedSLEDCalculationRequestId)
+      .getOrElse { throw IllegalStateException("PreviouslyRecordedSLED calculation request id didn't match a calculation request") }
+
+    return sourceDataMapper.mapAdjustments(calcRequest).none { it.adjustmentType == AdjustmentDto.AdjustmentType.UNUSED_DEDUCTIONS && (it.days ?: 0) > 0 }
   }
 
   private fun hasSedAndLedWithTheSameDate(dates: List<CalculationOutcome>): Boolean {
