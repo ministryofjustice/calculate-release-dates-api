36437f8010f625f16eb21ceff7ca5256ec50a715
james-baker-moj <159048573+james-baker-moj@users.noreply.github.com>
2025-12-08 13:39:04 +0000

    CRS-2553: Check for unused deductions on previous SLED calc (#1311)
    * CRS-2553: Check for unused deductions on previous SLED calc
    
    If the 2 day check calculation that we might use for a previously recorded SLED contains unused deductions then we do not use the previously recorded SLED.
    When someone comes back in on a recall and they have unused remand it means that their SLED will automatically be earlier than the previous SLED.
    
    * Fix import

--- src/test/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/PreviouslyRecordedSLEDServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/PreviouslyRecordedSLEDServiceTest.kt
@@ -1,19 +1,33 @@
 package uk.gov.justice.digital.hmpps.calculatereleasedatesapi.service
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.adjustmentsapi.model.AdjustmentDto
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.entity.CalculationOutcome
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.entity.CalculationRequest
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.enumerations.ReleaseDateType
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.PreviouslyRecordedSLED
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.repository.CalculationOutcomeRepository
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.repository.CalculationRequestRepository
 import java.time.LocalDate
+import java.util.Optional
 
-class DominantHistoricDateServiceTest {
+class PreviouslyRecordedSLEDServiceTest {
 
   private val calculationOutcomeRepository = mock<CalculationOutcomeRepository>()
-  private val service: DominantHistoricDateService = DominantHistoricDateService(calculationOutcomeRepository)
+  private val calculationRequestRepository = mock<CalculationRequestRepository>()
+  private val sourceDataMapper = mock<SourceDataMapper>()
+  private val service: PreviouslyRecordedSLEDService = PreviouslyRecordedSLEDService(calculationOutcomeRepository, calculationRequestRepository, sourceDataMapper)
+
+  @BeforeEach
+  fun setUp() {
+    whenever(calculationRequestRepository.findById(any())).thenReturn(Optional.of(CalculationRequest(id = 1L)))
+    whenever(sourceDataMapper.mapAdjustments(any())).thenReturn(emptyList())
+  }
 
   @Test
   fun `should return null if no historic SLED or SED + LED exists`() {
@@ -23,7 +37,69 @@ class DominantHistoricDateServiceTest {
   }
 
   @Test
-  fun `should return a previously recorded SLED if a historic SLED exists`() {
+  fun `should return a previously recorded SLED if a historic SLED exists and there are no unused deductions`() {
+    val outcomes = listOf(
+      CalculationOutcome(
+        calculationRequestId = 1L,
+        outcomeDate = LocalDate.of(2021, 1, 1),
+        calculationDateType = ReleaseDateType.SLED.name,
+      ),
+    )
+    whenever(calculationOutcomeRepository.getPotentialDatesForPreviouslyRecordedSLED("A1234BC", CALCULATED_SLED)).thenReturn(outcomes)
+    whenever(calculationRequestRepository.findById(any())).thenReturn(Optional.of(CalculationRequest(id = 1L)))
+    whenever(sourceDataMapper.mapAdjustments(any())).thenReturn(
+      listOf(
+        AdjustmentDto(
+          bookingId = 1L,
+          person = "A1234BC",
+          adjustmentType = AdjustmentDto.AdjustmentType.REMAND,
+          days = 5,
+        ),
+      ),
+    )
+
+    assertThat(service.findPreviouslyRecordedSLEDThatShouldOverrideTheCalculatedSLED(PRISONER_ID, CALCULATED_SLED)).isEqualTo(
+      PreviouslyRecordedSLED(
+        previouslyRecordedSLEDDate = LocalDate.of(2021, 1, 1),
+        calculatedDate = CALCULATED_SLED,
+        previouslyRecordedSLEDCalculationRequestId = 1L,
+      ),
+    )
+  }
+
+  @Test
+  fun `should return a previously recorded SLED if a historic SLED exists and there are unused deductions but days are 0`() {
+    val outcomes = listOf(
+      CalculationOutcome(
+        calculationRequestId = 1L,
+        outcomeDate = LocalDate.of(2021, 1, 1),
+        calculationDateType = ReleaseDateType.SLED.name,
+      ),
+    )
+    whenever(calculationOutcomeRepository.getPotentialDatesForPreviouslyRecordedSLED("A1234BC", CALCULATED_SLED)).thenReturn(outcomes)
+    whenever(calculationRequestRepository.findById(any())).thenReturn(Optional.of(CalculationRequest(id = 1L)))
+    whenever(sourceDataMapper.mapAdjustments(any())).thenReturn(
+      listOf(
+        AdjustmentDto(
+          bookingId = 1L,
+          person = "A1234BC",
+          adjustmentType = AdjustmentDto.AdjustmentType.UNUSED_DEDUCTIONS,
+          days = 0,
+        ),
+      ),
+    )
+
+    assertThat(service.findPreviouslyRecordedSLEDThatShouldOverrideTheCalculatedSLED(PRISONER_ID, CALCULATED_SLED)).isEqualTo(
+      PreviouslyRecordedSLED(
+        previouslyRecordedSLEDDate = LocalDate.of(2021, 1, 1),
+        calculatedDate = CALCULATED_SLED,
+        previouslyRecordedSLEDCalculationRequestId = 1L,
+      ),
+    )
+  }
+
+  @Test
+  fun `should not return a previously recorded SLED if the historic calculation had unused deductions`() {
     val outcomes = listOf(
       CalculationOutcome(
         calculationRequestId = 1L,
@@ -33,14 +109,19 @@ class DominantHistoricDateServiceTest {
     )
 
     whenever(calculationOutcomeRepository.getPotentialDatesForPreviouslyRecordedSLED("A1234BC", CALCULATED_SLED)).thenReturn(outcomes)
-
-    assertThat(service.findPreviouslyRecordedSLEDThatShouldOverrideTheCalculatedSLED(PRISONER_ID, CALCULATED_SLED)).isEqualTo(
-      PreviouslyRecordedSLED(
-        previouslyRecordedSLEDDate = LocalDate.of(2021, 1, 1),
-        calculatedDate = CALCULATED_SLED,
-        previouslyRecordedSLEDCalculationRequestId = 1L,
+    whenever(calculationRequestRepository.findById(any())).thenReturn(Optional.of(CalculationRequest(id = 1L)))
+    whenever(sourceDataMapper.mapAdjustments(any())).thenReturn(
+      listOf(
+        AdjustmentDto(
+          bookingId = 1L,
+          person = "A1234BC",
+          adjustmentType = AdjustmentDto.AdjustmentType.UNUSED_DEDUCTIONS,
+          days = 1,
+        ),
       ),
     )
+
+    assertThat(service.findPreviouslyRecordedSLEDThatShouldOverrideTheCalculatedSLED(PRISONER_ID, CALCULATED_SLED)).isNull()
   }
 
   @Test
