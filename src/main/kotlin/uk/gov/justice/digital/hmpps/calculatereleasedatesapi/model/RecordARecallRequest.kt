d4f331ec09ff28805e2da98f56c92a6f291dcd45
Yasin Mustafa <yasin.mustafa@gmail.com>
2026-01-09 08:20:14 +0000

    RCLL-643 Add return-to-custody-date for overlapping-adjustment check and also fix some issues with it

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/RecordARecallRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/RecordARecallRequest.kt
@@ -1,9 +1,12 @@
 package uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model
 
+import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDate
 import java.util.UUID
 
 data class RecordARecallRequest(
   val revocationDate: LocalDate,
+  @param:Schema(description = "The date the person was returned to custody. Optional if the person was already in prison at the revocation date ")
+  val returnToCustodyDate: LocalDate? = null,
   val recallId: UUID? = null,
 )
