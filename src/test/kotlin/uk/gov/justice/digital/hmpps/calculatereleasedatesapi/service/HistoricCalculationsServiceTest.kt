7800cfff62f6b8a8817630c90d9e0431632218e7
Faiz Ibrahim <faiz.ibrahim@digital.justice.gov.uk>
2026-02-02 10:51:52 +0000

    CRS-2604 Move to new ManageUsersAPI endpoint to get the usersDetails by usernames

--- src/test/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/HistoricCalculationsServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/service/HistoricCalculationsServiceTest.kt
@@ -4,14 +4,17 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.ArgumentMatchers.anyList
 import org.mockito.ArgumentMatchers.anyString
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.whenever
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.client.ManageUsersApiClient
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.entity.CalculationReason
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.entity.CalculationRequest
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.enumerations.CalculationStatus
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.manageusersapi.model.PrisonUserBasicDetails
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.Agency
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.CalculationSource
 import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.CalculationViewConfiguration
@@ -29,9 +32,35 @@ class HistoricCalculationsServiceTest {
   @Mock
   lateinit var prisonService: PrisonService
 
+  @Mock
+  lateinit var manageUsersApiClient: ManageUsersApiClient
+
   @InjectMocks
   lateinit var underTest: HistoricCalculationsService
   val reference: UUID = UUID.randomUUID()
+
+  private val usersDetails = mapOf(
+    "user" to PrisonUserBasicDetails(
+      username = "CRD_TEST_USER",
+      firstName = "Crd",
+      lastName = "Test User",
+      authSource = PrisonUserBasicDetails.AuthSource.nomis,
+      enabled = true,
+      staffId = 12345,
+      userId = 67890,
+      name = "Crd Test User",
+    ),
+    "user1" to PrisonUserBasicDetails(
+      username = "CRD_TEST_USER",
+      firstName = "Crd",
+      lastName = "Test User 1",
+      authSource = PrisonUserBasicDetails.AuthSource.nomis,
+      enabled = true,
+      staffId = 12345,
+      userId = 67890,
+      name = "Crd Test User",
+    ),
+  )
 
   @BeforeEach
   fun beforeEach() {
@@ -69,8 +98,10 @@ class HistoricCalculationsServiceTest {
   fun `Test source set to NOMIS if calculation not found in database`() {
     whenever(calculationRequestRepository.findAllByPrisonerIdAndCalculationStatus(anyString(), anyString())).thenReturn(listOf(calculationRequest()))
     whenever(prisonService.getCalculationsForAPrisonerId(anyString())).thenReturn(listOf(sentenceCalculationSummary("comment")))
+    whenever(manageUsersApiClient.getUsersByUsernames(anyList())).thenReturn(usersDetails)
     val result = underTest.getHistoricCalculationsForPrisoner("123")
     assertThat(result).hasSize(1)
+    assertThat(result[0].calculatedByDisplayName).isEqualTo("Crd Test User")
     assertThat(result[0].calculationSource).isEqualTo(CalculationSource.NOMIS)
     assertThat(result[0].calculationViewConfiguration).isNull()
     assertThat(result[0].establishment).isNull()
@@ -89,16 +120,19 @@ class HistoricCalculationsServiceTest {
     whenever(calculationRequestRepository.findAllByPrisonerIdAndCalculationStatus(anyString(), anyString())).thenReturn(listOf(calcRequest1, calcRequest2))
 
     val sentenceCalculationSummary1 = sentenceCalculationSummary("comment $reference")
-    val sentenceCalculationSummary2 = sentenceCalculationSummary1.copy(bookingId = 3, commentText = "comment: ${calcRequest2.calculationReference}")
+    val sentenceCalculationSummary2 = sentenceCalculationSummary1.copy(bookingId = 3, commentText = "comment: ${calcRequest2.calculationReference}", calculatedByUserId = "user1")
     whenever(prisonService.getCalculationsForAPrisonerId(anyString())).thenReturn(listOf(sentenceCalculationSummary1, sentenceCalculationSummary2))
+    whenever(manageUsersApiClient.getUsersByUsernames(anyList())).thenReturn(usersDetails)
     val result = underTest.getHistoricCalculationsForPrisoner("123")
     assertThat(result).hasSize(2)
+    assertThat(result[0].calculatedByDisplayName).isEqualTo("Crd Test User")
     assertThat(result[0].calculationSource).isEqualTo(CalculationSource.CRDS)
     assertThat(result[0].calculationViewConfiguration).isEqualTo(CalculationViewConfiguration(reference.toString(), 1))
     assertThat(result[0].establishment).isEqualTo("Chelmsford (HMP)")
     assertThat(result[0].genuineOverrideReasonCode).isEqualTo(GenuineOverrideReason.AGGRAVATING_FACTOR_OFFENCE)
     assertThat(result[0].genuineOverrideReasonDescription).isEqualTo("One or more offences have been characterised by an aggravating factor (such as terror)")
 
+    assertThat(result[1].calculatedByDisplayName).isEqualTo("Crd Test User 1")
     assertThat(result[1].calculationSource).isEqualTo(CalculationSource.CRDS)
     assertThat(result[1].calculationViewConfiguration).isEqualTo(CalculationViewConfiguration(calcRequest2.calculationReference.toString(), 1))
     assertThat(result[1].establishment).isEqualTo("HMP KENNET")
