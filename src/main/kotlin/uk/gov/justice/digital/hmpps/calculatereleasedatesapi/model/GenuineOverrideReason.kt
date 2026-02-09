b2a2a4af7a1d6f7bacb064529da8e3b23b7902a1
Faiz Ibrahim <faiz.ibrahim@digital.justice.gov.uk>
2026-02-09 10:55:46 +0000

    CRS-2595 Add - Enter approved dates - GenuineOverrideReason

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/GenuineOverrideReason.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/GenuineOverrideReason.kt
@@ -4,10 +4,11 @@ enum class GenuineOverrideReason(val description: String, val displayOrder: Int,
   TRIAL_RECORD_OR_BREAKDOWN_DOES_NOT_MATCH_OVERALL_SENTENCE_LENGTH("Trial record sheet/sentence breakdown doesn’t match overall sentence length", 0, false),
   RELEASE_DATE_FROM_ANOTHER_CUSTODY_PERIOD("Release dates (such as SLED or TUSED) from another period of custody are missing", 1, false),
   POWER_TO_DETAIN("An application for a power to detain has been approved", 2, false),
-  RELEASE_DATE_ON_WEEKEND_OR_HOLIDAY("The release dates fall on a weekend or public holiday", 3, false),
-  CROSS_BORDER_SECTION_RELEASE_DATE("The dates need to be replaced with cross border section release dates", 4, false),
-  AGGRAVATING_FACTOR_OFFENCE("One or more offences have been characterised by an aggravating factor (such as terror)", 5, false),
-  OTHER("The reason is not on the list", 5, true),
+  ENTER_APPROVED_DATES("Entering approved dates (such as APD, HDCAD and ROTL)", 3, false),
+  RELEASE_DATE_ON_WEEKEND_OR_HOLIDAY("The release dates fall on a weekend or public holiday", 4, false),
+  CROSS_BORDER_SECTION_RELEASE_DATE("The dates need to be replaced with cross border section release dates", 5, false),
+  AGGRAVATING_FACTOR_OFFENCE("One or more offences have been characterised by an aggravating factor (such as terror)", 6, false),
+  OTHER("The reason is not on the list", 6, true),
   ;
 
   fun toResponse() = GenuineOverrideReasonResponse(
