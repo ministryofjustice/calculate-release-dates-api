4febb3ee5e32665f065040f4c4a3b53fadf12ed8
Yasin Mustafa <yasin.mustafa@gmail.com>
2026-02-27 10:39:17 +0000

    RCLL-698 soften the recalls validation for the penultimate booking

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/RecordARecallValidationResult.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/RecordARecallValidationResult.kt
@@ -4,7 +4,36 @@ import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.validation.Validati
 import java.time.LocalDate
 
 data class RecordARecallValidationResult(
-  val criticalValidationMessages: List<ValidationMessage>,
-  val otherValidationMessages: List<ValidationMessage>,
+  val latestCriticalMessages: List<ValidationMessage> = emptyList(),
+  val latestOtherMessages: List<ValidationMessage> = emptyList(),
+  val penultimateCriticalMessages: List<ValidationMessage> = emptyList(),
+  val penultimateOtherMessages: List<ValidationMessage> = emptyList(),
+  val earliestSentenceDate: LocalDate,
+) {
+  companion object {
+    fun fromLatest(result: RecallInterimValidationResult): RecordARecallValidationResult =
+      RecordARecallValidationResult(
+        latestCriticalMessages = result.criticalMessages,
+        latestOtherMessages = result.otherMessages,
+        earliestSentenceDate = result.earliestSentenceDate,
+      )
+
+    fun fromLatestAndPenultimate(
+      latest: RecallInterimValidationResult,
+      penultimate: RecallInterimValidationResult
+    ): RecordARecallValidationResult =
+      RecordARecallValidationResult(
+        latestCriticalMessages = latest.criticalMessages,
+        latestOtherMessages = latest.otherMessages,
+        penultimateCriticalMessages = penultimate.criticalMessages,
+        penultimateOtherMessages = penultimate.otherMessages,
+        earliestSentenceDate = minOf(penultimate.earliestSentenceDate, latest.earliestSentenceDate),
+      )
+  }
+}
+
+data class RecallInterimValidationResult(
+  val criticalMessages: List<ValidationMessage>,
+  val otherMessages: List<ValidationMessage>,
   val earliestSentenceDate: LocalDate,
 )
