cfc2b9f3e96ec0f3dd47f9654433894c46cbf109
Matthew Green <matthew.green3@justice.gov.uk>
2026-04-15 11:07:11 +0100

    crs-2671-ftr56-tranche-boundary
    Remove manual testing code

--- src/test/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/calculation/StandardConfigurationTests.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/calculation/StandardConfigurationTests.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.calculatereleasedatesapi.calculation
 
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -10,16 +9,6 @@ import java.io.File
 import java.util.stream.Stream
 
 class StandardConfigurationTests : CalculationExampleTests() {
-
-  @Test
-  fun blah() {
-    // `Test Example`("ers0/crs-2642-ac1-1")
-    // `Test Example`("ftr-56/crs-2467-ac2-2")
-    // `Test Example`("ftr-56/blah") // should be T2 not T3
-    // `Test Example`("tranches/crs-2121_5x1")
-    // `Test Example`("ftr-56/crs-2462-ac1-2")
-    // `Test Example`("ftr-56/crs-2556-ac1-2")
-  }
 
   @ParameterizedTest
   @MethodSource(value = ["testCases"])
