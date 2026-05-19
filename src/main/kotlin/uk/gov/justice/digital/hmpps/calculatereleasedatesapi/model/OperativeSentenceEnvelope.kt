0197508690d70dd7e909866a9554b6aa3edcb44b
james-baker-moj <159048573+james-baker-moj@users.noreply.github.com>
2026-05-19 09:42:19 +0100

    CRS-2684: Update swagger for operative sentence envelope (#1515)
    The default docs made it look like you wouldn't get null from NOMIS for SDS+ and recall flags

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/OperativeSentenceEnvelope.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/model/OperativeSentenceEnvelope.kt
@@ -5,14 +5,14 @@ import java.time.LocalDate
 
 @Schema
 data class OperativeSentenceEnvelope(
-  @param:Schema(description = "The length in days of the sentence envelope")
+  @param:Schema(description = "The length in days of the sentence envelope", example = "365")
   val sentenceEnvelopeLengthInDays: Long,
-  @param:Schema(description = "The date of the earliest sentence in the envelope")
+  @param:Schema(description = "The date of the earliest sentence in the envelope", example = "2013-06-21")
   val earliestSentenceStartDate: LocalDate,
-  @param:Schema(description = "Whether this is for a post recall sentence envelope. Null indicates this is unknown.")
+  @param:Schema(description = "Whether this is for a post recall sentence envelope. Null indicates this is unknown.", examples = ["true", "false", "null"])
   val isPostRecallSentenceEnvelope: Boolean?,
-  @param:Schema(description = "Whether the sentence envelope contains an SDS plus sentence or not. Null indicates this is unknown.")
+  @param:Schema(description = "Whether the sentence envelope contains an SDS plus sentence or not. Null indicates this is unknown.", examples = ["true", "false", "null"])
   val containsAnSDSPlusSentence: Boolean?,
-  @param:Schema(description = "The source of the data used to determine the operative sentence envelope")
+  @param:Schema(description = "The source of the data used to determine the operative sentence envelope", example = "CRDS")
   val sentenceEnvelopeSource: OperativeSentenceEnvelopeSource,
 )
