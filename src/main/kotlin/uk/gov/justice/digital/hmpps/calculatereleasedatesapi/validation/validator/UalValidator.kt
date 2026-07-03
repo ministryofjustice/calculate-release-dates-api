c85084595b6e6d636955be6a87b6b2a49576728e
james-baker-moj <159048573+james-baker-moj@users.noreply.github.com>
2026-07-03 11:52:46 +0100

    CRS-2784: Overlapping UAL validation (#1568)
    This should not be possible to create using the adjustments service but we have seen instances where double-clicks and race conditions can lead to duplicate or overlapping UAL. These will be addressed separately but it's best to guarantee there is none present before performing a calculation as CRDS will produce an invalid date.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/validation/validator/UalValidator.kt
@@ -0,0 +1,30 @@
+package uk.gov.justice.digital.hmpps.calculatereleasedatesapi.validation.validator
+
+import org.springframework.stereotype.Component
+import org.threeten.extra.LocalDateRange
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.enumerations.AdjustmentType
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.model.Booking
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.validation.ValidationCode
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.validation.ValidationMessage
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.validation.ValidationOrder
+
+@Component
+class UalValidator : PreCalculationBookingValidator {
+
+  override fun validate(booking: Booking): List<ValidationMessage> {
+    val ual = booking.adjustments.getOrEmptyList(AdjustmentType.UNLAWFULLY_AT_LARGE)
+    if (ual.isEmpty()) {
+      return emptyList()
+    }
+    val messages = mutableListOf<ValidationMessage>()
+    val ualRanges = ual
+      .filter { it.fromDate != null && it.toDate != null }
+      .mapIndexed { index, adjustment -> index to LocalDateRange.of(adjustment.fromDate!!, adjustment.toDate!!) }
+    if (ualRanges.any { (index, ual) -> ualRanges.any { (otherIndex, otherUal) -> index != otherIndex && ual.isConnected(otherUal) } }) {
+      messages += ValidationMessage(ValidationCode.DUPLICATE_OR_OVERLAPPING_UAL)
+    }
+    return messages
+  }
+
+  override fun validationOrder() = ValidationOrder.INVALID
+}
