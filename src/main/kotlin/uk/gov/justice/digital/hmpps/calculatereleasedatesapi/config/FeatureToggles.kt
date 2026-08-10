f5427c1f09e2dfaf0d19e08865d336b7be4ebb70
James Baker <james.baker3@digital.justice.gov.uk>
2026-08-10 10:47:01 +0100

    CRS-2820: Config item API
    Small API to support showing the config for each environment

--- src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/config/FeatureToggles.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/calculatereleasedatesapi/config/FeatureToggles.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.calculatereleasedatesapi.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import uk.gov.justice.digital.hmpps.calculatereleasedatesapi.enumerations.ConfigItem
 
 @ConfigurationProperties(prefix = "feature-toggles")
 data class FeatureToggles(
@@ -17,4 +18,20 @@ data class FeatureToggles(
   var progressionTrancheOneManualJourney: Boolean = false,
   var progressionModelScheduleExclusionEnabled: Boolean = false,
   var routeProgressionModelScheduleExclusionToManual: Boolean = false,
-)
+) {
+  fun toConfigItems(): List<ConfigItem> = listOf(
+    ConfigItem("Support inactive sentences and adjustments", supportInactiveSentencesAndAdjustments.toString()),
+    ConfigItem("Use adjustments API", useAdjustmentsApi.toString()),
+    ConfigItem("Apply post recall repeal rules (disable TUSED)", applyPostRecallRepealRules.toString()),
+    ConfigItem("Record A Recall FTR56 Rules", recordARecallFtr56Rules.toString()),
+    ConfigItem("Store sentence level dates", storeSentenceLevelDates.toString()),
+    ConfigItem("Store operative sentence envelope for probation API", storeOperativeSentenceEnvelope.toString()),
+    ConfigItem("Apply post HDCED repeal rules (Adult HDC pre-PM still allowed)", applyPostHdcedRepealRules.toString()),
+    ConfigItem("Adult HDC suspended (clean stop)", adultHdcSuspended.toString()),
+    ConfigItem("Second check enabled", secondCheckEnabled.toString()),
+    ConfigItem("Route pre-progression extinguished sentence to manual (post-PM is too much remand validation error)", routePreProgressionExtinguishedSentenceToManual.toString()),
+    ConfigItem("Progression Model route T1 to manual", progressionTrancheOneManualJourney.toString()),
+    ConfigItem("Progression Model excluded offences enabled", progressionModelScheduleExclusionEnabled.toString()),
+    ConfigItem("Progression Model route excluded offences to manual", routeProgressionModelScheduleExclusionToManual.toString()),
+  )
+}
